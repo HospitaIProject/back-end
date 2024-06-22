e66c8a100d4733b85e9f27101aaffff20d98ed6c
Kim Dong Yeon <do121712@naver.com>
2024-06-23 03:42:10 +0900

    feat : 초기 User Entity에 기본 name 컬럼 추가

---
+++ src/main/java/com/team/hospital/user/User.java
@@ -0,0 +1,20 @@
+package com.team.hospital.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+}
