30e563c6bad381db0dfd23b0f7a55290c4a637cc
Kim Dong Yeon <do121712@naver.com>
2024-06-25 22:34:47 +0900

    feat : User를 Id로 조회하는 메소드와 없는 회원일 때의 예외처리 추가

--- src/main/java/com/team/hospital/user/UserService.java
+++ src/main/java/com/team/hospital/user/UserService.java
@@ -4,11 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public User findUserById(Long userId){
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty())
+            throw new IllegalArgumentException("회원 존재 x");
+        return user.get();
+    }
 
     @Transactional
     public void join(){
