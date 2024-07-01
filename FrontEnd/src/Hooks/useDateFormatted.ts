f9ec7da54505351176fdaaaa716fd05094400a00
jinhyuek <jjh943202@naver.com>
2024-07-02 01:47:35 +0900

    Feat: 날짜 포맷팅 커스텀훅 구현

---
+++ FrontEnd/src/Hooks/useDateFormatted.ts
@@ -0,0 +1,20 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko'); // 기본 로케일을 한국어로 설정합니다.
+
+export const useDateFormatted = (date: string) => {
+    const allDate = dayjs(date).format('YYYY년 MM월 DD일 HH시 mm분'); // 날짜와 시간을 표시합니다.
+    const relativeDate = dayjs(date).fromNow(); // 상대적인 시간을 표시합니다.
+    const onlyDate = dayjs(date).format('YYYY년 MM월 DD일'); // 날짜만 표시합니다.
+    const onlyTime = dayjs(date).format('HH시 mm분'); // 시간만 표시합니다.
+
+    return {
+        allDate,
+        relativeDate,
+        onlyDate,
+        onlyTime,
+    };
+};
