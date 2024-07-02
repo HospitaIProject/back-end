3f856d5341bf6bab6f6e4d000035099bbae7d635
jinhyuek <jjh943202@naver.com>
2024-07-03 03:54:42 +0900

    Refactor: ''값 들어갔을때 dateFormatted훅은 ''로 모두반환

--- FrontEnd/src/Hooks/useDateFormatted.ts
+++ FrontEnd/src/Hooks/useDateFormatted.ts
@@ -5,7 +5,14 @@ import 'dayjs/locale/ko';
 dayjs.extend(relativeTime);
 dayjs.locale('ko'); // 기본 로케일을 한국어로 설정합니다.
 
-export const useDateFormatted = (date: string) => {
+export const useDateFormatted = (date: Date | '') => {
+    if (!date)
+        return {
+            allDate: '',
+            relativeDate: '',
+            onlyDate: '',
+            onlyTime: '',
+        };
     const allDate = dayjs(date).format('YYYY년 MM월 DD일 HH시 mm분'); // 날짜와 시간을 표시합니다.
     const relativeDate = dayjs(date).fromNow(); // 상대적인 시간을 표시합니다.
     const onlyDate = dayjs(date).format('YYYY년 MM월 DD일'); // 날짜만 표시합니다.
