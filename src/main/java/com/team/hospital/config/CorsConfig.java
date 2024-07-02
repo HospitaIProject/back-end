ddab35b0215941a38eae4e534d091276393c4254
Kim Dong Yeon <do121712@naver.com>
2024-07-02 23:00:10 +0900

    feat : add cross-origin config

---
+++ src/main/java/com/team/hospital/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.team.hospital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization", "Accept")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
