3a13b298b4c2e114efd21ee74d7febfa08b247d5
Kim Dong Yeon <do121712@naver.com>
2024-07-04 05:25:45 +0900

    feat : add  CheckListItem repository

---
+++ src/main/java/com/team/hospital/api/checkListItem/CheckListItemRepository.java
@@ -0,0 +1,6 @@
+package com.team.hospital.api.checkListItem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckListItemRepository extends JpaRepository<CheckListItem, Long> {
+}
