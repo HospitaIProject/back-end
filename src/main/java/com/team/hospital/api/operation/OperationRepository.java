d2f21c457fe680bcfa37edb7f51b03195d387d6c
Kim Dong Yeon <do121712@naver.com>
2024-07-04 22:03:57 +0900

    feat : add to find all Operation by Patient method

--- src/main/java/com/team/hospital/api/operation/OperationRepository.java
+++ src/main/java/com/team/hospital/api/operation/OperationRepository.java
@@ -1,6 +1,11 @@
 package com.team.hospital.api.operation;
 
+import com.team.hospital.api.patient.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OperationRepository extends JpaRepository<Operation, Long>  {
+
+    List<Operation> findAllByPatient(Patient patient);
 }
