a84b1e53c23b3c700ea950fe731b6dbd9f099129
Kim Dong Yeon <do121712@naver.com>
2024-07-04 22:05:18 +0900

    feat : add to OperationResponse DTO and builder

--- src/main/java/com/team/hospital/api/operation/dto/OperationResponse.java
+++ src/main/java/com/team/hospital/api/operation/dto/OperationResponse.java
@@ -1,10 +1,17 @@
 package com.team.hospital.api.operation.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class OperationResponse {
 
     private OperationDTO operationDTO;
 
+    public static OperationResponse buildOperationResponse(OperationDTO operationDTO){
+        return OperationResponse.builder()
+                .operationDTO(operationDTO)
+                .build();
+    }
 }
