b178bed1a58b2c6f774efed2dfc2548a0f32e8db
Kim Dong Yeon <do121712@naver.com>
2024-07-05 19:56:35 +0900

    feat : add delete, modify api

--- src/main/java/com/team/hospital/api/operation/OperationController.java
+++ src/main/java/com/team/hospital/api/operation/OperationController.java
@@ -1,10 +1,10 @@
 package com.team.hospital.api.operation;
 
+import com.team.hospital.api.CommonApiResponse;
 import com.team.hospital.api.operation.dto.OperationDTO;
 import com.team.hospital.api.operation.dto.OperationResponse;
 import com.team.hospital.api.operation.dto.RegisterOperation;
-import com.team.hospital.api.patient.Patient;
-import com.team.hospital.api.patient.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,21 +19,32 @@ public class OperationController {
     private final OperationService operationService;
 
     @PostMapping("/api/operation/{patientId}")
-    public ResponseEntity<?> save(@RequestBody RegisterOperation registerOperation,
-                                  @PathVariable Long patientId){
+    @Operation(summary = "특정 환자에 대한 operation 등록", description = "입력한 환자의 ID값에 해당한 환자의 operation 등록")
+    public CommonApiResponse<?> save(@RequestBody RegisterOperation registerOperation,
+                                     @PathVariable Long patientId){
         operationService.save(registerOperation, patientId);
-        return ResponseEntity.status(HttpStatus.OK).body("success");
+        return CommonApiResponse.createSuccess();
     }
 
     @GetMapping("/api/operations/{patientId}")
-    public ResponseEntity<List<OperationDTO>> findOperations(@PathVariable Long patientId) {
+    @Operation(summary = "특정 환자에 대한 operation 목록", description = "입력한 환자의 ID값에 해당하는 환자의 operation 목록")
+    public CommonApiResponse<List<OperationDTO>> findOperations(@PathVariable Long patientId) {
         List<OperationDTO> operationDTOS = operationService.findAllByPatient(patientId);
-        return ResponseEntity.status(HttpStatus.OK).body(operationDTOS);
+        return CommonApiResponse.createSuccess(operationDTOS);
     }
 
-    @GetMapping("/api/operation/{operationId}")
-    public ResponseEntity<OperationResponse> findOperation(@PathVariable Long operationId){
-        OperationDTO operationDTO = OperationDTO.buildOperationDTO(operationService.findOperationById(operationId));
-        return ResponseEntity.status(HttpStatus.OK).body(OperationResponse.buildOperationResponse(operationDTO));
+    @PutMapping("/api/operation/{operationId}")
+    @Operation(summary = "operation 수정")
+    public CommonApiResponse<?> modifyOperation(@RequestBody RegisterOperation registerOperation,
+                                                             @PathVariable Long operationId){
+        operationService.modify(registerOperation, operationId);
+        return CommonApiResponse.createSuccess();
+    }
+
+    @DeleteMapping("/api/operation/{operationId}")
+    @Operation(summary = "operation 삭제")
+    public CommonApiResponse<?> deleteOperation(@PathVariable Long operationId){
+        operationService.delete(operationId);
+        return CommonApiResponse.createSuccess();
     }
 }
