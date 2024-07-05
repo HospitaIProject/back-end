79dce864eff520096dd5ee39e003531294e7cdad
Kim Dong Yeon <do121712@naver.com>
2024-07-05 19:58:22 +0900

    feat : add info, delete, modify api

--- src/main/java/com/team/hospital/api/checkList/CheckListController.java
+++ src/main/java/com/team/hospital/api/checkList/CheckListController.java
@@ -1,8 +1,10 @@
 package com.team.hospital.api.checkList;
 
+import com.team.hospital.api.CommonApiResponse;
 import com.team.hospital.api.checkList.dto.CheckListDTO;
 import com.team.hospital.api.checkList.dto.CheckListResponse;
 import com.team.hospital.api.checkList.dto.WriteCheckList;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,34 +19,46 @@ public class CheckListController {
     private final CheckListService checkListService;
 
     @PostMapping("/api/checkList/{checkListItemId}")
-    public ResponseEntity<?> save(@RequestBody WriteCheckList writeCheckList,
-                                            @PathVariable Long checkListItemId){
+    @Operation(summary = "세팅한 체크리스트에 대하여 체크리스트 등록")
+    public CommonApiResponse<?> save(@RequestBody WriteCheckList writeCheckList,
+                                     @PathVariable Long checkListItemId) {
         checkListService.save(writeCheckList, checkListItemId);
-        return ResponseEntity.status(HttpStatus.OK).body("success");
+        return CommonApiResponse.createSuccess();
     }
 
     @GetMapping("/api/checkLists/{operationId}")
-    public ResponseEntity<List<CheckListDTO>> findCheckListByOperationId(@PathVariable Long operationId){
+    @Operation(summary = "operation에 해당하는 체크리스트 목록")
+    public CommonApiResponse<List<CheckListDTO>> findCheckListByOperationId(@PathVariable Long operationId) {
         List<CheckListDTO> list = checkListService.findAllByOperation(operationId);
-        return ResponseEntity.status(HttpStatus.OK).body(list);
+        return CommonApiResponse.createSuccess(list);
     }
 
     @GetMapping("/api/checkList")
-    public ResponseEntity<List<CheckListDTO>> findAllCheckList(){
+    @Operation(summary = "전체 체크리스트 조회")
+    public CommonApiResponse<List<CheckListDTO>> findAllCheckList() {
         List<CheckListDTO> list = checkListService.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(list);
+        return CommonApiResponse.createSuccess(list);
     }
 
     @GetMapping("/api/checkList/{checkListId}")
-    public ResponseEntity<CheckListResponse> findCheckList(@PathVariable Long checkListId){
-        CheckListDTO checkListDTO = CheckListDTO.buildComplianceDTO(checkListService.findCheckListById(checkListId));
-        return ResponseEntity.status(HttpStatus.OK).body(CheckListResponse.buildCheckListResponse(checkListDTO));
+    @Operation(summary = "체크리스트 상세 조회")
+    public CommonApiResponse<CheckListResponse> findCheckList(@PathVariable Long checkListId) {
+        CheckListDTO checkListDTO = CheckListDTO.toEntity(checkListService.findCheckListById(checkListId));
+        return CommonApiResponse.createSuccess(CheckListResponse.toEntity(checkListDTO));
     }
 
-    @PutMapping("/api/{complianceId}")
-    public ResponseEntity<?> modifyCheckList(@RequestBody WriteCheckList writeCheckList,
-                                              @PathVariable Long complianceId){
-        checkListService.modify(writeCheckList, complianceId);
-        return ResponseEntity.status(HttpStatus.OK).body("success");
+    @PutMapping("/api/{checkListId}")
+    @Operation(summary = "체크리스트 수정")
+    public CommonApiResponse<?> modifyCheckList(@RequestBody WriteCheckList writeCheckList,
+                                             @PathVariable Long checkListId) {
+        checkListService.modify(writeCheckList, checkListId);
+        return CommonApiResponse.createSuccess();
+    }
+
+    @DeleteMapping("/api/{checkListId}")
+    @Operation(summary = "체크리스트 삭제")
+    public CommonApiResponse<?> deleteCheckList(@PathVariable Long checkListId) {
+        checkListService.delete(checkListId);
+        return CommonApiResponse.createSuccess();
     }
 }
