0ccd050657b6948ab76ae777fcc75447cbc81800
jinhyuek <jjh943202@naver.com>
2024-07-08 06:09:08 +0900

    Refactor: datapicker  ui수정

--- FrontEnd/src/components/common/datePicker/DatePickerContainer.tsx
+++ FrontEnd/src/components/common/datePicker/DatePickerContainer.tsx
@@ -3,10 +3,10 @@ import { CalendarContainer } from 'react-datepicker';
 const MyContainer = ({ className, children }: { className?: any; children: React.ReactNode }) => {
     return (
         <CalendarContainer className={className}>
-            <div className="flex w-full flex-col">
+            <div className="flex flex-col w-full">
                 <div style={{ position: 'relative' }}>{children}</div>
-                <div className="mt-2 flex flex-row items-center gap-2 pl-5">
-                    <div className="h-3 w-3 rounded-lg bg-blue-500" />
+                <div className="flex flex-row items-center gap-2 pl-5 mt-2">
+                    <div className="w-3 h-3 bg-blue-500 rounded-lg" />
                     <span>Today</span>
                 </div>
                 {/* <span className="flex flex-row justify-end w-full pr-4">최대 3개월 이내의 날짜만 신청 가능합니다.</span> */}
--- FrontEnd/src/components/common/datePicker/DatePickerModal.tsx
+++ FrontEnd/src/components/common/datePicker/DatePickerModal.tsx
@@ -21,53 +21,53 @@ function DatePickerModal({ initialDate, onClose, onSelectDate }: Props) {
         if (selectDate === null) return;
         onSelectDate(selectDate);
     };
-    const handleNowDate = () => {
-        setSelectDate(new Date());
-    };
+
     useEffect(() => {
         if (initialDate) setSelectDate(initialDate);
     }, [initialDate]);
 
     return (
         <ModalFullScreenContainer title="날짜 선택" onClose={onClose} maxWidthClassName="max-w-[650px]">
-            <DatePicker
-                selected={selectDate}
-                locale={ko}
-                onChange={(date) => setSelectDate(date)}
-                inline
-                calendarContainer={MyContainer}
-                disabledKeyboardNavigation
-                scrollableYearDropdown
-                showYearDropdown
-                yearDropdownItemNumber={15} // 현재 년도를 중심으로 앞뒤로 15년을 표시
-                renderCustomHeader={({
-                    date,
-                    changeYear,
-                    changeMonth,
-                    decreaseMonth,
-                    increaseMonth,
-                    prevMonthButtonDisabled,
-                    nextMonthButtonDisabled,
-                }) => (
-                    <DatePickerHeader
-                        date={date}
-                        changeYear={changeYear}
-                        changeMonth={changeMonth}
-                        decreaseMonth={decreaseMonth}
-                        increaseMonth={increaseMonth}
-                        prevMonthButtonDisabled={prevMonthButtonDisabled}
-                        nextMonthButtonDisabled={nextMonthButtonDisabled}
-                    />
-                )}
-            />
-            <div className="sticky bottom-0 mt-auto flex w-full justify-end bg-white p-5">
-                <button
-                    onClick={handleChangeDate}
-                    type="button"
-                    className="rounded-md bg-blue-500 px-8 py-2 text-white hover:bg-blue-600"
-                >
-                    선택하기
-                </button>
+            <div className="flex flex-col w-full">
+                <DatePicker
+                    selected={selectDate}
+                    locale={ko}
+                    onChange={(date) => setSelectDate(date)}
+                    inline
+                    calendarContainer={MyContainer}
+                    disabledKeyboardNavigation
+                    scrollableYearDropdown
+                    showYearDropdown
+                    yearDropdownItemNumber={15} // 현재 년도를 중심으로 앞뒤로 15년을 표시
+                    renderCustomHeader={({
+                        date,
+                        changeYear,
+                        changeMonth,
+                        decreaseMonth,
+                        increaseMonth,
+                        prevMonthButtonDisabled,
+                        nextMonthButtonDisabled,
+                    }) => (
+                        <DatePickerHeader
+                            date={date}
+                            changeYear={changeYear}
+                            changeMonth={changeMonth}
+                            decreaseMonth={decreaseMonth}
+                            increaseMonth={increaseMonth}
+                            prevMonthButtonDisabled={prevMonthButtonDisabled}
+                            nextMonthButtonDisabled={nextMonthButtonDisabled}
+                        />
+                    )}
+                />
+                <div className="sticky bottom-0 flex justify-end w-full p-5 mt-auto bg-white">
+                    <button
+                        onClick={handleChangeDate}
+                        type="button"
+                        className="px-8 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                    >
+                        선택하기
+                    </button>
+                </div>
             </div>
         </ModalFullScreenContainer>
     );
