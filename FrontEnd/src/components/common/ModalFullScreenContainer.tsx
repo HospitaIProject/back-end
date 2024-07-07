df8a00bb3af29f53f29e2ece0f59795ebf17fa7f
jinhyuek <jjh943202@naver.com>
2024-07-08 06:08:51 +0900

    Refactor: fullsreen modal 최대넓이, 백그라운드 색상 수정

--- FrontEnd/src/components/common/ModalFullScreenContainer.tsx
+++ FrontEnd/src/components/common/ModalFullScreenContainer.tsx
@@ -8,9 +8,10 @@ type ModalWrapperProps = {
 
     onClose?: () => void;
     maxWidthClassName?: string; //container className
+    bgClassName?: string; //background className
 };
 
-function ModalFullScreenContainer({ children, title, onClose, maxWidthClassName }: ModalWrapperProps) {
+function ModalFullScreenContainer({ children, title, onClose, maxWidthClassName, bgClassName }: ModalWrapperProps) {
     const [isBrowser, setIsBrowser] = useState(false);
 
     useEffect(() => {
@@ -20,19 +21,19 @@ function ModalFullScreenContainer({ children, title, onClose, maxWidthClassName 
     if (!open || !isBrowser) return null;
 
     return ReactDOM.createPortal(
-        <div className="bg-modal fixed bottom-0 left-0 right-0 top-0 z-[2000] flex h-full w-screen justify-center overflow-y-auto bg-white pt-4">
+        <div className="bg-modal fixed bottom-0 left-0 right-0 top-0 z-[2000] flex h-dvh w-screen justify-center bg-gray-800 bg-opacity-40 pt-10">
             <div
-                className={`flex w-full ${maxWidthClassName ? maxWidthClassName : 'max-w-[1300px]'} flex-col rounded-t-lg border-x border-t`}
+                className={`flex w-full scroll-smooth ${maxWidthClassName ? maxWidthClassName : 'max-w-screen-tablet'} flex-col rounded-t-lg border-x border-t ${bgClassName ? bgClassName : 'bg-white'}`}
             >
-                <header className="flex w-full flex-row justify-between p-6 text-gray-600">
-                    <div className="ml-7 flex flex-grow justify-center font-medium">
+                <header className="flex flex-row justify-between w-full p-6 text-gray-600">
+                    <div className="flex justify-center flex-grow font-medium ml-7">
                         <span className="text-lg">{title}</span>
                     </div>
                     <button onClick={onClose}>
-                        <CloseIcon className="h-8 w-8 text-inherit" />
+                        <CloseIcon className="w-8 h-8 text-inherit" />
                     </button>
                 </header>
-                {children}
+                <div className="w-full overflow-y-auto">{children}</div>
             </div>
         </div>,
         document.getElementById('modal-root') as HTMLElement,
