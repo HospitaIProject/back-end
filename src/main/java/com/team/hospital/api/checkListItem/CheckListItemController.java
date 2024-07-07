f36826579ee67a15aebd2cc36c49b5bb59bc8ca5
Kim Dong Yeon <do121712@naver.com>
2024-07-07 16:45:38 +0900

    chore : 주석 및 공백 제거

--- src/main/java/com/team/hospital/api/checkListItem/CheckListItemController.java
+++ src/main/java/com/team/hospital/api/checkListItem/CheckListItemController.java
@@ -6,7 +6,6 @@ import com.team.hospital.api.checkListItem.dto.CheckListItemResponse;
 import com.team.hospital.api.checkListItem.dto.WriteCheckListItem;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,14 +17,14 @@ public class CheckListItemController {
     @PostMapping("/api/checkListItem/{operationId}")
     @Operation(summary = "operation에 대한 checkList 세팅", description = "입력한 operation의 ID값에 해당한 operation의 checkListItem 등록")
     public CommonApiResponse<?> save(@RequestBody WriteCheckListItem writeCheckListItem,
-                                          @PathVariable Long operationId){
+                                     @PathVariable Long operationId) {
         checkListItemService.save(writeCheckListItem, operationId);
         return CommonApiResponse.createSuccess();
     }
 
     @GetMapping("/api/checkListItem/{operationId}")
     @Operation(summary = "operation에 대한 checkList 세팅 조회")
-    public CommonApiResponse<CheckListItemResponse> findByOperation(@PathVariable Long operationId){
+    public CommonApiResponse<CheckListItemResponse> findByOperation(@PathVariable Long operationId) {
         CheckListItemDTO checkListItemDTO = CheckListItemDTO.toEntity(checkListItemService.findCheckListItemByOperation(operationId));
         return CommonApiResponse.createSuccess(CheckListItemResponse.toEntity(checkListItemDTO));
     }
@@ -33,14 +32,14 @@ public class CheckListItemController {
     @PutMapping("/api/checkListItem/{checkListItemId}")
     @Operation(summary = "세팅된 checkListItem 수정")
     public CommonApiResponse<?> modifyCheckListItem(@RequestBody WriteCheckListItem writeCheckListItem,
-                                                 @PathVariable Long checkListItemId){
+                                                    @PathVariable Long checkListItemId) {
         checkListItemService.modify(writeCheckListItem, checkListItemId);
         return CommonApiResponse.createSuccess();
     }
 
     @DeleteMapping("/api/checkListItem/{checkListItemId}")
     @Operation(summary = "세팅된 checkListItem 세팅 삭제")
-    public CommonApiResponse<?> deleteCheckListItem(@PathVariable Long checkListItemId){
+    public CommonApiResponse<?> deleteCheckListItem(@PathVariable Long checkListItemId) {
         checkListItemService.delete(checkListItemId);
         return CommonApiResponse.createSuccess();
     }
