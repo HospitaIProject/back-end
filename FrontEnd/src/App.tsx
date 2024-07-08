551cdab6cc0109af293da42e6769fe809ff4e676
jinhyuek <jjh943202@naver.com>
2024-07-09 02:36:40 +0900

    Feat: 라우트 추가 NewPatientFormPage,SurgeryListPage,CheckListsPage,NewSurgeryInfoFormPage

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -3,9 +3,12 @@ import MainPage from './MainPage/MainPage';
 import LoginPage from './LoginPage/LoginPage';
 import Layout from './Layout/Layout';
 import HeaderLayout from './Layout/HeaderLayout';
-import NewPatientFormPage from './PatientPage/NewPatientFormPage/NewPatientFormPage';
+import NewSurgeryInfoFormPage from './PatientPage/NewSurgeryInfoFormPage/NewSurgeryInfoFormPage';
 import ComplianceFormPage from './PatientPage/ComplianceFormPage/ComplianceFormPage';
 import RQProviders from './components/RQProvider';
+import NewPatientFormPage from './PatientPage/NewPatientFormPage/NewPatientFormPage';
+import SurgeryListPage from './PatientPage/SurgeryPage/SurgeryListPage';
+import CheckListsPage from './PatientPage/CheckListsPage/CheckListsPage';
 
 function App() {
     return (
@@ -13,17 +16,23 @@ function App() {
             <Routes>
                 <Route element={<RQProviders />}>
                     {/* 전체 레이아웃 */}
-
                     <Route element={<Layout />}>
                         {/* 헤더 레이아웃 */}
                         <Route element={<HeaderLayout />}>
                             {/* 메인 페이지 */}
                             <Route path="/" element={<MainPage />} />
                             {/* 뒤로가기 */}
-                            {/* 환자 등록 페이지 */}
-                            <Route path="/patient/new" element={<NewPatientFormPage />} />
+                            {/* 수술정보 리스트 페이지 */}
+                            <Route path="/patient/surgery/list" element={<SurgeryListPage />} />
+
+                            <Route path="/patient/checkLists" element={<CheckListsPage />} />
+
+                            {/* 환자 정보 등록 페이지 */}
+                            <Route path="/patient/new/info" element={<NewPatientFormPage />} />
+                            {/* 환자 수술정보 등록 페이지 */}
+                            <Route path="/patient/new/surgery" element={<NewSurgeryInfoFormPage />} />
                             {/* compilance 체크리스트 페이지 */}
-                            <Route path="/patient/form/compilance/:id" element={<ComplianceFormPage />} />
+                            <Route path="/patient/form/compliance" element={<ComplianceFormPage />} />
                         </Route>
                         <Route path="/login" element={<LoginPage />} />
                     </Route>
