7d0d96f257ceea206ca940fc9b261039c25352f1
jinhyuek <jjh943202@naver.com>
2024-07-09 02:53:20 +0900

    Refactor: promise 타입 변경 CheckListsItemType->ComplianceValuesType

--- FrontEnd/src/PatientPage/_lib/checkListsService.ts
+++ FrontEnd/src/PatientPage/_lib/checkListsService.ts
@@ -1,15 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import Axios from '../../utils/axiosInstance';
-import { CheckListsItemType } from '../../models/CheckListsType';
+import { ComplianceValuesType } from '../../models/FormType';
 
-const getCheckLists = async (surgeryId: number): Promise<CheckListsItemType[]> => {
+const getCheckLists = async (surgeryId: number): Promise<ComplianceValuesType[]> => {
     const response = await Axios.get(`api/checkLists/${surgeryId}`);
     return response.data.data;
 };
 export const useCheckListsQuery = ({ surgeryId }: { surgeryId: number }) => {
-    const query = useQuery<CheckListsItemType[]>({
+    const query = useQuery<ComplianceValuesType[]>({
         queryKey: ['checklists', surgeryId],
         queryFn: () => getCheckLists(surgeryId),
     });
+
     return query;
 };
