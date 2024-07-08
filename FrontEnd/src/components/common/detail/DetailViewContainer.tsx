c60a300ad9763e55332e127c9223bbf20aef77d4
jinhyuek <jjh943202@naver.com>
2024-07-09 01:59:58 +0900

    Refactor: DetailView컴포넌트 간격 수정

--- FrontEnd/src/components/common/detail/DetailViewContainer.tsx
+++ FrontEnd/src/components/common/detail/DetailViewContainer.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function DetailViewContainer({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col w-full gap-1 pt-1 bg-gray-100">
-            <div className="flex flex-row items-center justify-between w-full gap-3 p-3 bg-white border-y">
+            <div className="flex flex-row items-center justify-between w-full gap-3 px-5 py-2 bg-white border-y">
                 <span className="text-gray-600">{/* 환자명:&nbsp;<span className="">{patientName}</span> */}</span>
                 <div className="flex flex-row gap-3">
                     <Link to="" className="text-gray-400">
