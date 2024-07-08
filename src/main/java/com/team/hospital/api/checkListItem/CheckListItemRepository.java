580f14881dba92b1c5ceb59550737cf98f6dba62
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-08 05:00:44 -0400

    refactor: findCheckListItemByOperation query logic edited.

--- src/main/java/com/team/hospital/api/checkListItem/CheckListItemRepository.java
+++ src/main/java/com/team/hospital/api/checkListItem/CheckListItemRepository.java
@@ -1,11 +1,10 @@
 package com.team.hospital.api.checkListItem;
 
-import com.team.hospital.api.operation.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CheckListItemRepository extends JpaRepository<CheckListItem, Long> {
 
-    Optional<CheckListItem> findCheckListItemByOperation(Operation operation);
+    Optional<CheckListItem> findCheckListItemByOperationId(Long operationId);
 }
