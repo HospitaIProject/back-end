66a053e408c14f7c095556fbc7ba0981f5ad3263
jinhyuek <jjh943202@naver.com>
2024-07-09 10:11:12 +0900

    Refactor: 삭제, 수정 버튼 동작 제거. 메인페이지  임시 서버오류 컴포넌트 구현, PatientSummaryCard  등록번호 label 다음줄로 개행되는현상 수정

--- FrontEnd/src/components/common/detail/DetailViewContainer.tsx
+++ FrontEnd/src/components/common/detail/DetailViewContainer.tsx
@@ -7,14 +7,14 @@ function DetailViewContainer({ children }: { children: React.ReactNode }) {
             <div className="flex flex-row items-center justify-between w-full gap-3 px-5 py-2 bg-white border-y">
                 <span className="text-gray-600">{/* 환자명:&nbsp;<span className="">{patientName}</span> */}</span>
                 <div className="flex flex-row gap-3">
-                    <Link to="" className="text-gray-400">
+                    <button className="text-gray-400">
                         <span>삭제</span>
-                    </Link>
+                    </button>
                     <span className="text-gray-400">|</span>
 
-                    <Link className="text-green-500" to="">
+                    <button className="text-green-500">
                         <span>수정</span>
-                    </Link>
+                    </button>
                 </div>
             </div>
             <div className="flex flex-col w-full p-4 bg-white">{children}</div>
