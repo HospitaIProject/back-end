dd17cceafaf54e494801714d3e41111cacb8271a
jinhyuek <jjh943202@naver.com>
2024-07-11 01:45:25 +0900

    Init: 필터 api 연동  세팅

--- FrontEnd/src/MainPage/_lib/patientService.ts
+++ FrontEnd/src/MainPage/_lib/patientService.ts
@@ -2,14 +2,36 @@ import { useQuery } from '@tanstack/react-query';
 import Axios from '../../utils/axiosInstance';
 import { PatientWithOperationDtoType } from '../../models/PatientType';
 
-const getPatientList = async (): Promise<PatientWithOperationDtoType[]> => {
+const SC_MATCH_ITEMS: { [key: string]: string | undefined } = {
+    patientName: 'PATIENT_NAME',
+    patientNumber: 'PATIENT_NUMBER',
+    surgeryMethod: 'OPERATION_METHOD',
+    '': undefined,
+};
+
+const getPatientList = async ({ q, sc }: { q: string; sc: string }): Promise<PatientWithOperationDtoType[]> => {
+    const params = {
+        filterType: SC_MATCH_ITEMS[q],
+        query: sc,
+    };
+    console.log('params', params);
+
     const response = await Axios.get('api/patients');
     return response.data.data;
 };
-export const usePatientListQuery = () => {
+export const usePatientListQuery = (searchParams: URLSearchParams) => {
+    // const page = Number(searchParams.get('page')) || 1;
+    const q = searchParams.get('q') || '';
+    const sc = searchParams.get('sc') || '';
+    // const sort = searchParams.get('sort') || '';
+
     const query = useQuery<PatientWithOperationDtoType[]>({
-        queryKey: ['patient', 'list'],
-        queryFn: getPatientList,
+        queryKey: ['patient', 'list', q, sc],
+        queryFn: () =>
+            getPatientList({
+                q,
+                sc,
+            }),
     });
     return query;
 };
