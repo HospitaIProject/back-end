7cad7085e30097057cdc7028a0a15d80a5c77cc2
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-10 12:47:45 -0400

    feat: findOperationsByOperationMethod changed to return Slice<Operation>.

--- src/main/java/com/team/hospital/api/operation/OperationRepository.java
+++ src/main/java/com/team/hospital/api/operation/OperationRepository.java
@@ -1,6 +1,8 @@
 package com.team.hospital.api.operation;
 
 import com.team.hospital.api.patient.Patient;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,7 @@ public interface OperationRepository extends JpaRepository<Operation, Long>  {
 
     List<Operation> findAllByPatient(Patient patient);
 
-    List<Operation> findOperationsByOperationMethod(String operationMethod);
+//    List<Operation> findOperationsByOperationMethod(String operationMethod);
+
+    Slice<Operation> findOperationsByOperationMethod(String operationMethod, Pageable pageable);
 }
