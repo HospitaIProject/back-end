f65e5ebb14b2698a2ce3c09886c232c64ef01129
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-12 12:06:30 -0400

    feat: Update Operation field values to reflect new requirements

--- src/main/java/com/team/hospital/api/operation/dto/OperationDTO.java
+++ src/main/java/com/team/hospital/api/operation/dto/OperationDTO.java
@@ -2,13 +2,14 @@ package com.team.hospital.api.operation.dto;
 
 import com.team.hospital.api.checkList.enumType.BooleanOption;
 import com.team.hospital.api.operation.Operation;
-import com.team.hospital.api.operation.enumType.ASAScore;
-import com.team.hospital.api.operation.enumType.StomaFormation;
+import com.team.hospital.api.operation.enumType.OperationApproach;
+import com.team.hospital.api.operation.enumType.OperationMethod;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
@@ -17,66 +18,36 @@ public class OperationDTO {
 
     private Long operationId;
 
-    private float height;
+    private List<OperationMethod> operationMethod;        // 수술 방법
 
-    private float weight;
+    private List<String> customOperationMethod;           // 사용자 정의 수술 방법
 
-    private float BMI;
+    private OperationApproach operationApproach;            // 수술 approach
 
-    private ASAScore asaScore;      // 마취전 건강 상태 평가 점수
+    private BooleanOption stomaFormation;               // 장루 형성술
 
-    private String location;        // 위치
+    private LocalDateTime operationStartTime;     // 수술 시작 시간
 
-    private String dignosis;        // 진단
+    private LocalDateTime operationEndTime;     // 수술 시작 시간
 
-    private Date operationDate;    // 수술일
+    private int totalOperationTime;             // 전체 수술 시간 (분)
 
-    private Date hospitalizedDate;  // 입원일
+    private double totalFluidsAmount;             // 수술 중 총 들어간 수액( crystalloid) 양 (cc)
 
-    private Date dischargedDate;    // 퇴원일
-
-    private int totalHospitalizedDays;  // 총 재원일 수
-
-    private String operationMethod;      // 수술 방법
-
-    private String operationApproach;   // 수술 approach
-
-    private StomaFormation stomaFormation;      // 장루 형성술
-
-    private String ajcCStage;           // 암 진행도
-
-    private int numberOfRetrievedLine;  // 제거된 림프절 개수
-
-    private BooleanOption complicationOccurence;
-
-    private String cdClassification;
-
-    private BooleanOption reOperationWithIn30Days;
-
-    private String reOperationCause;
+    private double bloodLoss;             // 수술 중 실혈양 (cc)
 
     public static OperationDTO toEntity(Operation operation){
         return OperationDTO.builder()
                 .operationId(operation.getId())
-                .height(operation.getHeight())
-                .weight(operation.getWeight())
-                .BMI(operation.getBmi())
-                .asaScore(operation.getAsaScore())
-                .location(operation.getLocation())
-                .dignosis(operation.getDignosis())
-                .operationDate(operation.getOperationDate())
-                .hospitalizedDate(operation.getHospitalizedDate())
-                .dischargedDate(operation.getDischargedDate())
-                .totalHospitalizedDays(operation.getTotalHospitalizedDays())
                 .operationMethod(operation.getOperationMethod())
+                .customOperationMethod(operation.getCustomOperationMethod())
                 .operationApproach(operation.getOperationApproach())
                 .stomaFormation(operation.getStomaFormation())
-                .ajcCStage(operation.getAjcCStage())
-                .numberOfRetrievedLine(operation.getNumberOfRetrievedLine())
-                .complicationOccurence(operation.getComplicationOccurence())
-                .cdClassification(operation.getCdClassification())
-                .reOperationWithIn30Days(operation.getReOperationWithIn30Days())
-                .reOperationCause(operation.getReOperationCause())
+                .operationStartTime(operation.getOperationStartTime())
+                .operationEndTime(operation.getOperationEndTime())
+                .totalOperationTime(operation.getTotalOperationTime())
+                .totalFluidsAmount(operation.getTotalFluidsAmount())
+                .bloodLoss(operation.getBloodLoss())
                 .build();
     }
 
