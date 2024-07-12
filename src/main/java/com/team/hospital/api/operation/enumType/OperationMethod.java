263c4b2912502ffcc97a4e618dfb90087f937784
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-12 12:06:48 -0400

    feat: OperationMethod enum type added.

---
+++ src/main/java/com/team/hospital/api/operation/enumType/OperationMethod.java
@@ -0,0 +1,15 @@
+package com.team.hospital.api.operation.enumType;
+
+public enum OperationMethod {
+
+    RHC_ERHC,
+    T_COLECTOMY,
+    LHC_ELHC,
+    AR,
+    LAR,
+    ISR,
+    APR,
+    SUBTOTAL_TOTAL_COLECTOMY,
+    TOTAL_PROCTOCOLECTOMY
+
+}
