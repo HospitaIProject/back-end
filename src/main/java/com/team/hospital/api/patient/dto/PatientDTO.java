bc46cf956d0242a011864450034b6c963b9bfdbb
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-12 12:08:00 -0400

    feat: Update Patient field values to reflect new requirements.

--- src/main/java/com/team/hospital/api/patient/dto/PatientDTO.java
+++ src/main/java/com/team/hospital/api/patient/dto/PatientDTO.java
@@ -1,5 +1,8 @@
 package com.team.hospital.api.patient.dto;
 
+import com.team.hospital.api.operation.enumType.ASAScore;
+import com.team.hospital.api.operation.enumType.Dignosis;
+import com.team.hospital.api.operation.enumType.Location;
 import com.team.hospital.api.patient.Patient;
 import com.team.hospital.api.patient.enumType.Sex;
 import jakarta.persistence.EnumType;
@@ -8,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @Getter
 @Builder
@@ -15,14 +19,33 @@ public class PatientDTO {
 
     private Long patientId;
 
-    private Long patientNumber;
+    private Long patientNumber;                 // 등록번호
 
-    private String name;
+    private String name;                        // 환자이름
 
-    @Enumerated(EnumType.STRING)
-    private Sex sex;
+    private Sex sex;                            // 성별
 
-    private LocalDate birthday;
+    private int age;                            // 나이
+
+    private float height;                       // 키(cm)
+
+    private float weight;                       // 몸무게(kg)
+
+    private float bmi;                          // BMI(kg/cm²)
+
+    private ASAScore asaScore;                  // 마취전 건강 상태 평가 점수
+
+    private Location location;                  // 위치
+
+    private Dignosis dignosis;                  // 진단명
+
+    private Date operationDate;                 // 수술일
+
+    private Date hospitalizedDate;              // 입원일
+
+    private Date dischargedDate;                // 퇴원일
+
+    private int totalHospitalizedDays;          // 총 재원 일수
 
     public static PatientDTO createPatientDTO(Patient patient) {
         return PatientDTO.builder()
@@ -30,7 +53,17 @@ public class PatientDTO {
                 .patientNumber(patient.getPatientNumber())
                 .name(patient.getName())
                 .sex(patient.getSex())
-                .birthday(patient.getBirthday())
+                .age(patient.getAge())
+                .height(patient.getHeight())
+                .weight(patient.getWeight())
+                .bmi(patient.getBmi())
+                .asaScore(patient.getAsaScore())
+                .location(patient.getLocation())
+                .dignosis(patient.getDignosis())
+                .operationDate(patient.getOperationDate())
+                .hospitalizedDate(patient.getHospitalizedDate())
+                .dischargedDate(patient.getDischargedDate())
+                .totalHospitalizedDays(patient.getTotalHospitalizedDays())
                 .build();
     }
 }
