cb8aee593524c0c3938eac1133e7411ac2c73b1d
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-13 22:14:58 -0400

    feat: CheckListBeforeResponse added.

---
+++ src/main/java/com/team/hospital/api/checkList/checkListBefore/CheckListBeforeResponse.java
@@ -0,0 +1,18 @@
+package com.team.hospital.api.checkList.checkListBefore;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CheckListBeforeResponse {
+
+    private CheckListBeforeDTO checkListBeforeDTO;
+
+    public static CheckListBeforeResponse toEntity(CheckListBeforeDTO checkListBeforeDTO){
+        return CheckListBeforeResponse.builder()
+                .checkListBeforeDTO(checkListBeforeDTO)
+                .build();
+
+    }
+}
