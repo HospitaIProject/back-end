e0e7d636673dd6b09dec0038148f5e6b4a3539a4
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-13 22:14:48 -0400

    feat: CheckListBeforeRepository added.

---
+++ src/main/java/com/team/hospital/api/checkList/checkListBefore/CheckListBeforeRepository.java
@@ -0,0 +1,6 @@
+package com.team.hospital.api.checkList.checkListBefore;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CheckListBeforeRepository extends JpaRepository<CheckListBefore, Long> {
+}
