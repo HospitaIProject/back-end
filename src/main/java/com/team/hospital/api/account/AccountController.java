0a17db068e358c22bd5f558d069722451f1c12a0
Kim Dong Yeon <do121712@naver.com>
2024-07-15 22:20:35 +0900

    feat : add AccountController and save Account api

---
+++ src/main/java/com/team/hospital/api/account/AccountController.java
@@ -0,0 +1,23 @@
+package com.team.hospital.api.account;
+
+import com.team.hospital.api.SuccessResponse;
+import com.team.hospital.api.account.dto.CreateAccountRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/api/account")
+    @Operation(summary = "최초 계정 등록")
+    public SuccessResponse<?> save(@RequestBody CreateAccountRequest request) {
+        accountService.save(request);
+        return SuccessResponse.createSuccess();
+    }
+}
