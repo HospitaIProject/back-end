bbacd0628de6abd5eafbccb5252feb0ae6c4629f
jinhyuek <jjh943202@naver.com>
2024-07-15 20:36:52 +0900

    Feat: disabled 기능 추가 수술전, 수술중 , 수술후 각각다른 인풋을 위해

--- FrontEnd/src/components/common/form/input/YesOrNoButton.tsx
+++ FrontEnd/src/components/common/form/input/YesOrNoButton.tsx
@@ -6,25 +6,41 @@ type Props<T> = {
     htmlFor: string;
     formik: FormikProps<T>;
     isRender?: boolean;
+    isDisabled?: boolean;
+    etcComponent?: React.ReactNode;
 };
 
-function YesOrNoButton<T>({ label, htmlFor, formik, isRender = true }: Props<T>) {
+function YesOrNoButton<T>({ label, htmlFor, formik, isDisabled = false, isRender = true, etcComponent }: Props<T>) {
     const handleChange = (e: string) => {
         formik?.setFieldValue(htmlFor, e);
         formik?.setFieldError(htmlFor, '');
     };
 
     const selectedValue = formik?.getFieldProps(htmlFor).value as 'YES' | 'NO' | '';
-    const isInput = formik?.getFieldProps(htmlFor).value === 'YES' || formik?.getFieldProps(htmlFor).value === 'NO';
+    let isInput = false;
+
     const isError = (formik.errors as Record<string, string>)[htmlFor]; // formik의 에러 여부
 
+    if (formik?.getFieldProps(htmlFor).value === 'YES' && etcComponent) {
+        isInput = formik?.getFieldProps(htmlFor).value === 'YES' && formik?.getFieldProps(`${htmlFor}Date`).value;
+    } else {
+        isInput = formik?.getFieldProps(htmlFor).value === 'YES' || formik?.getFieldProps(htmlFor).value === 'NO';
+    }
     return (
-        <InputContainer<T> isRender={isRender} label={label} htmlFor={htmlFor} isInput={isInput} formik={formik}>
+        <InputContainer<T>
+            isRender={isRender}
+            etcComponent={etcComponent}
+            label={label}
+            htmlFor={htmlFor}
+            isInput={isInput}
+            formik={formik}
+        >
             <div
                 className={`flex h-12 max-w-60 flex-grow overflow-hidden rounded-lg border bg-gray-100 ${isError ? 'border-2 border-red-400' : ''} `}
             >
                 <button
                     type="button"
+                    disabled={isDisabled}
                     className={`flex-1 border-r ${selectedValue === 'YES' ? 'bg-blue-500 text-white' : 'text-blue-400'}`}
                     onClick={() => handleChange('YES')}
                 >
@@ -32,6 +48,7 @@ function YesOrNoButton<T>({ label, htmlFor, formik, isRender = true }: Props<T>)
                 </button>
                 <button
                     type="button"
+                    disabled={isDisabled}
                     className={`flex-1 border-l ${selectedValue === 'NO' ? 'bg-blue-500 text-white' : 'text-red-400'}`}
                     onClick={() => handleChange('NO')}
                 >
