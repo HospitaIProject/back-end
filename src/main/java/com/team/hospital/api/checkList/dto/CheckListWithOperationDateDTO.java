bd802ef67e32b2c5716429085d5183e30bc7d362
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-16 14:14:27 -0400

    feat: toEntity method edited to contain CheckListBeforeDTO, CheckListDuringDTO.

--- src/main/java/com/team/hospital/api/checkList/dto/CheckListWithOperationDateDTO.java
+++ src/main/java/com/team/hospital/api/checkList/dto/CheckListWithOperationDateDTO.java
@@ -1,6 +1,8 @@
 package com.team.hospital.api.checkList.dto;
 
 import com.team.hospital.api.checkList.CheckList;
+import com.team.hospital.api.checkListBefore.CheckListBeforeDTO;
+import com.team.hospital.api.checkListDuring.CheckListDuringDTO;
 import com.team.hospital.api.operation.dto.OperationDTO;
 import com.team.hospital.api.operation.dto.OperationDateDTO;
 import com.team.hospital.api.patient.Patient;
@@ -16,12 +18,24 @@ public class CheckListWithOperationDateDTO {
 
     private List<CheckListDTO> checkListDTOs;
     private OperationDateDTO operationDateDTO;
+    private CheckListBeforeDTO checkListBeforeDTO;
+    private CheckListDuringDTO checkListDuringDTO;
     private boolean checkListCreatedToday;
 
     public static CheckListWithOperationDateDTO toEntity(List<CheckList> checkLists, OperationDTO operationDTO, Patient patient, boolean checkListCreatedToday) {
         return CheckListWithOperationDateDTO.builder()
                 .checkListDTOs(checkLists.stream().map(CheckListDTO::toEntity).toList())
                 .operationDateDTO(OperationDateDTO.toEntity(operationDTO, PatientDTO.createPatientDTO(patient)))
+                .checkListCreatedToday(checkListCreatedToday)
+                .build();
+    }
+
+    public static CheckListWithOperationDateDTO toEntity(List<CheckList> checkLists, OperationDTO operationDTO, CheckListBeforeDTO checkListBeforeDTO, CheckListDuringDTO checkListDuringDTO, Patient patient, boolean checkListCreatedToday) {
+        return CheckListWithOperationDateDTO.builder()
+                .checkListDTOs(checkLists.stream().map(CheckListDTO::toEntity).toList())
+                .operationDateDTO(OperationDateDTO.toEntity(operationDTO, PatientDTO.createPatientDTO(patient)))
+                .checkListBeforeDTO(checkListBeforeDTO)
+                .checkListDuringDTO(checkListDuringDTO)
                 .checkListCreatedToday(checkListCreatedToday)
                 .build();
     }
