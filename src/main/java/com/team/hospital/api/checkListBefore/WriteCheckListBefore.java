f61d2f9b6d95ae278678dbf3a5b3b2c50f414384
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-16 14:11:30 -0400

    feat: WriteCheckListBefore class added.

--- src/main/java/com/team/hospital/api/checkListBefore/WriteCheckListBefore.java
+++ src/main/java/com/team/hospital/api/checkListBefore/WriteCheckListBefore.java
@@ -1,4 +1,4 @@
-package com.team.hospital.api.checkList.checkListBefore;
+package com.team.hospital.api.checkListBefore;
 
 import com.team.hospital.api.checkList.enumType.BooleanOption;
 import lombok.Getter;
