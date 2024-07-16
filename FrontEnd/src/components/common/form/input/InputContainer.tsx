d706ea3da044edcc4bc345f8c52550b6d8e43a4a
jinhyuek <jjh943202@naver.com>
2024-07-16 20:12:56 +0900

    Feat: 기존 Input 컴폰넌트에 기타 컴포넌트 추가할 수 있게 구현

--- FrontEnd/src/components/common/form/input/InputContainer.tsx
+++ FrontEnd/src/components/common/form/input/InputContainer.tsx
@@ -13,9 +13,11 @@ type Props<T> = {
     formik: FormikProps<T>;
     isRemark?: boolean;
     isRender?: boolean;
+
+    etcComponent?: React.ReactNode;
 };
 
-function InputContainer<T>({ label, htmlFor, children, isInput, formik, isRender = true }: Props<T>) {
+function InputContainer<T>({ label, htmlFor, children, isInput, formik, isRender = true, etcComponent }: Props<T>) {
     const [isRemarkOpen, setIsRemarkOpen] = useState(false); // 비고 입력창 열림 여부
 
     const handleOpenRemark = () => {
@@ -24,7 +26,7 @@ function InputContainer<T>({ label, htmlFor, children, isInput, formik, isRender
 
     let isRemarkButtonRender = false; // 비고 버튼 렌더링 여부
     let remarkValue = ''; // 비고 입력창의 값
-    const remarkKey = `${htmlFor}_remark`; // 비고 입력창의 key
+    const remarkKey = `${htmlFor}_remarks`; // 비고 입력창의 key
 
     if (typeof formik?.values === 'object' && formik?.values !== null) {
         if (Object.prototype.hasOwnProperty.call(formik.values, remarkKey)) {
@@ -42,7 +44,7 @@ function InputContainer<T>({ label, htmlFor, children, isInput, formik, isRender
         <div
             className={`${isRender ? '' : 'hidden'} mx-auto flex w-full max-w-screen-mobile flex-col items-center px-2`}
         >
-            <div className="flex flex-col w-full gap-2 mb-2 mobile:flex-row">
+            <div className="mb-2 flex w-full flex-col gap-2 mobile:flex-row">
                 <label
                     className={`flex items-center gap-2 pr-2 mobile:w-72 mobile:flex-shrink-0 ${isInput ? 'text-blue-500' : 'text-gray-700'}`}
                     htmlFor={htmlFor}
@@ -54,16 +56,17 @@ function InputContainer<T>({ label, htmlFor, children, isInput, formik, isRender
                         />
                     }
                 </label>
-                <div className="flex flex-row w-full gap-2 mobile:justify-between">
-                    <div className="flex flex-col flex-grow gap-1 pr-2">
+                <div className="flex w-full flex-row gap-2 mobile:justify-between">
+                    <div className="flex flex-grow flex-col gap-1 pr-2">
                         {children}
                         {isValid && (
                             <div className="flex flex-row items-center gap-1 px-2 text-red-500">
-                                <InfoIcons className="w-4 h-4 text-inherit" />
+                                <InfoIcons className="h-4 w-4 text-inherit" />
                                 <small className="text-xs text-inherit">{isValid}</small>
                             </div>
                         )}
                     </div>
+                    {etcComponent ? etcComponent : ''}
                     <button
                         type="button"
                         //키에remark 없을시 hidden
@@ -79,6 +82,7 @@ function InputContainer<T>({ label, htmlFor, children, isInput, formik, isRender
             </div>
             <textarea
                 onChange={debouncedHandleChange}
+                defaultValue={remarkValue}
                 className={`w-full rounded-md border border-gray-300 p-2 text-sm font-medium outline-none ${isRemarkOpen ? '' : 'hidden'}`}
             />
         </div>
