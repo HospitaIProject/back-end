c091a2a72e2fa183a5d6b3f93af43178031bba31
jinhyuek <jjh943202@naver.com>
2024-07-16 20:15:37 +0900

    Feat: checkListForm 제출시  불필요한 필드 제거

--- FrontEnd/src/PatientPage/_lib/checkListsService.ts
+++ FrontEnd/src/PatientPage/_lib/checkListsService.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import Axios from '../../utils/axiosInstance';
-import { ResponseCheckListType } from '../../models/CheckListsType';
+import { CheckListsBeforeItemType, CheckListsDuringItemType, ResponseCheckListType } from '../../models/CheckListsType';
+import { AxiosError } from 'axios';
+import { ErrorResponseType } from '../../models/AxiosResponseType';
 
 const getCheckLists = async (operationId: number): Promise<ResponseCheckListType> => {
     const response = await Axios.get(`api/checkLists/${operationId}`);
@@ -15,3 +17,46 @@ export const useCheckListsQuery = ({ operationId }: { operationId: number }) => 
 
     return query;
 }; //체크리스트 가져오기 커스텀 훅
+
+const getCheckListBefore = async (operationId: number): Promise<CheckListsBeforeItemType> => {
+    const response = await Axios.get(`api/checkListBeforeDetail/${operationId}`);
+    return response.data.data;
+};
+const getCheckListDuring = async (operationId: number): Promise<CheckListsDuringItemType> => {
+    const response = await Axios.get(`api/checkListDuringDetail/${operationId}`);
+    return response.data.data;
+};
+// const getCheckListAfter = async (operationId: number): Promise<CheckListsAfterItemType> => {
+//     const response = await Axios.get(`api/checkListBefore/${operationId}`);
+//     return response.data.data;
+// };
+
+export const useCheckListBeforeOperationQuery = ({
+    operationId,
+    enabled = true,
+}: {
+    operationId: number;
+    enabled?: boolean;
+}) => {
+    const query = useQuery<CheckListsBeforeItemType, AxiosError<ErrorResponseType>>({
+        queryKey: ['checklistBeforeOperation', operationId],
+        queryFn: () => getCheckListBefore(operationId),
+        enabled: enabled,
+    });
+
+    return query;
+};
+export const useCheckListDuringOperationQuery = ({
+    operationId,
+    enabled = true,
+}: {
+    operationId: number;
+    enabled?: boolean;
+}) => {
+    const query = useQuery<CheckListsDuringItemType, AxiosError<ErrorResponseType>>({
+        queryKey: ['getCheckListDuringOperation', operationId],
+        queryFn: () => getCheckListDuring(operationId),
+        enabled: enabled,
+    });
+    return query;
+};
