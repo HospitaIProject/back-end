8da1d114506e827be2118b4d90a98562a84b09a2
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-17 12:10:14 -0400

    feat: findByOperationId method added.

--- src/main/java/com/team/hospital/api/complication/ComplicationController.java
+++ src/main/java/com/team/hospital/api/complication/ComplicationController.java
@@ -2,6 +2,7 @@ package com.team.hospital.api.complication;
 
 import com.team.hospital.api.apiResponse.SuccessResponse;
 import com.team.hospital.api.checkList.enumType.BooleanOption;
+import com.team.hospital.api.complication.dto.ComplicationDTO;
 import com.team.hospital.api.complication.dto.WriteComplication;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,6 +15,13 @@ import org.springframework.web.bind.annotation.*;
 public class ComplicationController {
 
     private final ComplicationService complicationService;
+
+    @GetMapping("/api/complication/{operationId}")
+    @Operation(summary = "operation에 대한 complication 조회", description = "입력한 operation의 ID값에 해당하는 operation의 complication 조회")
+    public SuccessResponse<?> findByOperationId(@PathVariable Long operationId) {
+        Complication complication = complicationService.findComplicationByOperationId(operationId);
+        return SuccessResponse.createSuccess(ComplicationDTO.toEntity(complication));
+    }
 
     @PostMapping("/api/complication/{operationId}")
     @Operation(summary = "operation에 대한 complication 세팅", description = "입력한 operation의 ID값에 해당하는 operation의 complication 등록")
@@ -35,6 +43,13 @@ public class ComplicationController {
     @Operation(summary = "operation에 대한 complication status 세팅 (YES / NO)", description = "입력한 operation의 ID값에 해당하는 operation의 complication 여부 세팅")
     public SuccessResponse<?> setComplicationStatus(@PathVariable Long operationId, @RequestParam BooleanOption booleanOption) {
         complicationService.updateComplicationStatus(operationId, booleanOption);
+        return SuccessResponse.createSuccess();
+    }
+
+    @DeleteMapping("/api/complication/{operationId}")
+    @Operation(summary = "operation에 대한 complication 삭제", description = "입력한 operation의 ID값에 해당하는 operation의 complication 삭제")
+    public SuccessResponse<?> deleteComplication(@PathVariable Long operationId) {
+        complicationService.delete(operationId);
         return SuccessResponse.createSuccess();
     }
 }
