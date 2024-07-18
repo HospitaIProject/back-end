84efaf0974f6b8fbec50cd94bcbedba0dd26b6ce
jinhyuek <jjh943202@naver.com>
2024-07-19 08:17:36 +0900

    Feat: 체크리스트 작성시 오늘 작성했다는 표시 뜨는 기능 추가

--- FrontEnd/src/PatientPage/CheckListsPage/components/CheckListsSummaryCard.tsx
+++ FrontEnd/src/PatientPage/CheckListsPage/components/CheckListsSummaryCard.tsx
@@ -11,6 +11,7 @@ import { useDateFormatted } from '../../../Hooks/useDateFormatted';
 import ArrowIcon from '../../../icons/ArrowIcon';
 import useOperationDayFormat from '../../../Hooks/useOperationDateFormatted';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import SparklingIcon from '../../../icons/SparklingIcon';
 // import { Link } from 'react-router-dom';
 
 type Props = {
@@ -44,8 +45,9 @@ function CheckListsSummaryCard({
     const { pathname } = useLocation();
     const { createAt, updatedAt } = checkListData;
 
-    const { allDate: formattedCreateAt } = useDateFormatted(createAt || ''); //작성일
+    const { allDate: formattedCreateAt, relativeDate } = useDateFormatted(createAt || ''); //작성일
     const { allDate: formattedUpdatedAte } = useDateFormatted(updatedAt || ''); //수정일
+    const { dateComparison: createAtComparison } = useDateFormatted(createAt);
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -76,10 +78,10 @@ function CheckListsSummaryCard({
     return (
         <>
             <li
-                className="flex w-full cursor-pointer flex-row items-center border-y bg-white p-4 hover:bg-blue-50"
+                className="relative flex w-full cursor-pointer flex-row items-center border-y bg-white p-4 hover:bg-blue-50"
                 onClick={() => openModal()}
             >
-                <div className="flex w-full flex-row items-center gap-4">
+                <div className="flex w-full flex-row items-center gap-6">
                     <span className="text-lg font-semibold text-sky-800">{dateComparison}</span>
                     <div className="flex flex-col gap-1">
                         <span className="inline-block break-words text-sm text-gray-700">
@@ -97,6 +99,12 @@ function CheckListsSummaryCard({
                 <button className="text-sm font-semibold text-blue-600 underline underline-offset-4">
                     <ArrowIcon className="h-5 w-5 text-blue-600" />
                 </button>
+                {createAtComparison === 'TODAY' && (
+                    <div className="absolute left-0 top-0 flex flex-row items-center gap-1 rounded-br-lg bg-yellow-200 p-[3px] font-serif text-xs text-gray-700">
+                        <span>Today</span>
+                        <SparklingIcon className="h-3 w-3 text-yellow-700" />
+                    </div>
+                )}
             </li>
             {isModalOpen && (
                 <CheckListsDetailModal
