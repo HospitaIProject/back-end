bca73022c308b43fba5419a173d25324dff4df34
jinhyuek <jjh943202@naver.com>
2024-07-21 08:46:52 +0900

    Refactor: textarea 추가

--- FrontEnd/src/components/common/form/input/TextInput.tsx
+++ FrontEnd/src/components/common/form/input/TextInput.tsx
@@ -1,23 +1,35 @@
 import { FormikProps } from 'formik';
 import InputContainer from './InputContainer';
 import { useState } from 'react';
+import { debounce } from 'lodash';
 
 type Props<T> = {
     label: string;
     htmlFor: string;
     formik: FormikProps<T>;
     isRender?: boolean;
+    type?: 'textarea' | 'text';
 };
 
-function TextInput<T>({ label, htmlFor, formik, isRender }: Props<T>) {
+function TextInput<T>({ label, htmlFor, type = 'text', formik, isRender }: Props<T>) {
     const [isFocused, setIsFocused] = useState(false);
 
     const handleFocus = () => setIsFocused(true);
     const handleBlur = () => setIsFocused(false);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         formik?.setFieldValue(htmlFor, e.target.value);
         formik?.setFieldError(htmlFor, '');
     };
+    const handleChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        formik?.setFieldValue(htmlFor, e.target.value);
+        formik?.setFieldError(htmlFor, '');
+    };
+    const debouncedHandleChange = debounce((e: React.ChangeEvent<HTMLInputElement>) => handleChange(e), 500);
+    const debouncedHandleChangeTextArea = debounce(
+        (e: React.ChangeEvent<HTMLTextAreaElement>) => handleChangeTextArea(e),
+        500,
+    );
 
     // const selectedValue = formik?.getFieldProps(htmlFor).value as 'YES' | 'NO' | '';
     const isInput = formik?.getFieldProps(htmlFor).value;
@@ -26,15 +38,28 @@ function TextInput<T>({ label, htmlFor, formik, isRender }: Props<T>) {
     return (
         <InputContainer<T> isRender={isRender} label={label} htmlFor={htmlFor} isInput={isInput} formik={formik}>
             <div className="flex flex-grow overflow-hidden">
-                <input
-                    type="text"
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    defaultValue={formik?.getFieldProps(htmlFor).value}
-                    placeholder="텍스트를 입력 해주세요."
-                    className={`h-12 w-full outline-none ${isFocused ? 'border-2 border-blue-400' : ''} rounded-lg border border-gray-300 px-2 ${isValid ? 'border-2 border-red-400' : ''}`}
-                />
+                {type === 'text' && (
+                    <input
+                        type="text"
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        onChange={debouncedHandleChange}
+                        defaultValue={formik?.getFieldProps(htmlFor).value}
+                        placeholder="텍스트를 입력 해주세요."
+                        className={`h-12 w-full outline-none ${isFocused ? 'border-2 border-blue-400' : ''} rounded-lg border border-gray-300 px-2 ${isValid ? 'border-2 border-red-400' : ''}`}
+                    />
+                )}
+                {type === 'textarea' && (
+                    <textarea
+                        rows={3}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        onChange={debouncedHandleChangeTextArea}
+                        defaultValue={formik?.getFieldProps(htmlFor).value}
+                        placeholder="텍스트를 입력 해주세요."
+                        className={`w-full outline-none ${isFocused ? 'border-2 border-blue-400' : ''} rounded-lg border border-gray-300 px-2 ${isValid ? 'border-2 border-red-400' : ''}`}
+                    />
+                )}
             </div>
         </InputContainer>
     );
