dcf7fdea39b0d3f0de0c46470a409f0ca0ac3715
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-20 07:04:47 -0400

    feat: existsByOperation method added.

--- src/main/java/com/team/hospital/api/complication/ComplicationRepository.java
+++ src/main/java/com/team/hospital/api/complication/ComplicationRepository.java
@@ -1,5 +1,6 @@
 package com.team.hospital.api.complication;
 
+import com.team.hospital.api.operation.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,6 @@ public interface ComplicationRepository extends JpaRepository<Complication, Long
 
     @Query("SELECT cl From Complication cl where cl.operation.id = :operationId")
     Optional<Complication> findByOperationId(@Param("operationId") Long operationId);
+
+    boolean existsByOperation(Operation operation);
 }
