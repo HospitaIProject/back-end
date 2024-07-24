95d134c17ad0f40b9b69d6dc17f65a4414c2c118
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-07-24 15:44:29 -0400

    fix: Exclude health check endpoints from forwarding in ForwardingController
    - Updated ForwardingController to exclude /hc and /env endpoints from being forwarded.
    - Added condition to check request URI and forward specific paths directly.

--- src/main/java/com/team/hospital/webcontroller/ForwardingController.java
+++ src/main/java/com/team/hospital/webcontroller/ForwardingController.java
@@ -1,5 +1,6 @@
 package com.team.hospital.webcontroller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ForwardingController {
 
     @RequestMapping(value = "/**/{path:[^\\.]*}")
-    public String redirect() {
+    public String redirect(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+
+        if (requestURI.startsWith("/hc") || requestURI.startsWith("/env")) {
+            return "forward:" + requestURI;
+        }
+
         // Forward to home page so that route is preserved.
         return "forward:/";
     }
