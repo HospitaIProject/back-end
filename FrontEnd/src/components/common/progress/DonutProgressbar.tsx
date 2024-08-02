e937fe892b1273f956af609f4f9676bc16110b11
jinhyuek <jjh943202@naver.com>
2024-08-03 05:13:42 +0900

    Feat: compliance percentage -operation 목록에 보일수 있도록 도넛차트 추가 -도넛 차트 색상, text size 받을수 있도록 수정

--- FrontEnd/src/components/common/progress/DonutProgressbar.tsx
+++ FrontEnd/src/components/common/progress/DonutProgressbar.tsx
@@ -1,7 +1,47 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import ChangingProgressProvider from './ChangingProgressProvider';
 
-function DonutProgressbar({ percent, className, unit }: { percent: number; className: string; unit: string }) {
+const COLORS = {
+    First: {
+        pathColor: '#5F81FF',
+        trailColor: '#DFE8FF',
+    },
+    Second: {
+        trailColor: '#EAE3FF',
+        pathColor: '#8161DB',
+    },
+    Third: {
+        trailColor: '#E3F7FF',
+        pathColor: '#6183DB',
+    },
+    Fourth: {
+        trailColor: '#E3FFF3',
+        pathColor: '#61DBB7',
+    },
+    Fifth: {
+        trailColor: '#E3FAFF',
+        pathColor: '#61CDDB',
+    },
+    Sixth: {
+        trailColor: '#FFE3E3',
+        pathColor: '#DB6161',
+    },
+};
+
+function DonutProgressbar({
+    percent,
+    className,
+    unit,
+    color,
+    textClassName,
+}: {
+    percent: number;
+    className: string;
+    unit: string;
+    color: string;
+    textClassName: string;
+}) {
+    const progressColor = COLORS[color as keyof typeof COLORS];
     return (
         <div className={`className relative flex items-center justify-center ${className}`}>
             <ChangingProgressProvider values={[0, percent]}>
@@ -11,18 +51,17 @@ function DonutProgressbar({ percent, className, unit }: { percent: number; class
                         strokeWidth={13}
                         className="progressbar"
                         styles={buildStyles({
-                            textSize: '1rem',
                             strokeLinecap: 'round',
                             pathTransitionDuration: 0.5,
 
                             textColor: '#2B2D36',
-                            pathColor: '#61DBB7',
-                            trailColor: '#E3FFF3',
+                            pathColor: progressColor.pathColor,
+                            trailColor: progressColor.trailColor,
                         })}
                     />
                 )}
             </ChangingProgressProvider>
-            <span className="absolute text-xs font-semibold text-gray-700">
+            <span className={`absolute ${textClassName} font-medium text-gray-700`}>
                 {percent}
                 {unit}
             </span>
