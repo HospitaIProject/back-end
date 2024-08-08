076878a3dca567118a547d97bfc763130d4a1fd6
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-08-07 23:52:38 -0400

    feat: filtering by CheckListStatus feature added in findPatients method.

--- src/main/java/com/team/hospital/api/patient/PatientController.java
+++ src/main/java/com/team/hospital/api/patient/PatientController.java
@@ -6,9 +6,11 @@ import com.team.hospital.api.operation.Operation;
 import com.team.hospital.api.operation.OperationService;
 import com.team.hospital.api.operation.dto.OperationDTO;
 import com.team.hospital.api.patient.dto.PatientDTO;
-import com.team.hospital.api.patient.dto.RegisterPatient;
 import com.team.hospital.api.patient.dto.PatientWithOperationDateDTO;
+import com.team.hospital.api.patient.dto.RegisterPatient;
+import com.team.hospital.api.patient.enumType.CheckListStatus;
 import com.team.hospital.api.patient.enumType.FilterType;
+import com.team.hospital.api.patient.enumType.Opdate;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -18,9 +20,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -47,33 +47,6 @@ public class PatientController {
         return SuccessResponse.createSuccess(patientDTO);
     }
 
-    @GetMapping("/patients")
-    @io.swagger.v3.oas.annotations.Operation(summary = "필터 환자 조회", description = "필터를 이용해 환자를 조회합니다.")
-    public SuccessResponse<?> findPatients(@RequestParam(required = false) FilterType filterType,
-                                           @RequestParam(required = false) String query,
-                                           @RequestParam(required = false) Integer page,
-                                           @RequestParam(required = false) Integer size) {
-        Pageable pageable = PageRequest.of(page != null ? page - 1 : 0, size != null ? size : 10);
-
-        List<PatientWithOperationDateDTO> patientDTOs;
-        boolean queryPresent = StringUtils.hasText(query) && filterType != null;
-        Slice<Patient> filteredPatients = queryPresent ? getFilteredPatients(filterType, query, pageable) : null;
-
-        if (queryPresent && !filteredPatients.isEmpty()) {
-            patientDTOs = filteredPatients.getContent().stream()
-                    .sorted(Comparator.comparing(Patient::getUpdatedAt).reversed())
-                    .map(this::convertToPatientWithOperationDateDTO)
-                    .toList();
-        } else {
-            patientDTOs = patientService.findAll(pageable).stream()
-                    .sorted(Comparator.comparing(Patient::getUpdatedAt).reversed())
-                    .map(this::convertToPatientWithOperationDateDTO)
-                    .toList();
-        }
-
-        return SuccessResponse.createSuccess(patientDTOs);
-    }
-
     @PutMapping("/patient/{patientId}")
     @io.swagger.v3.oas.annotations.Operation(summary = "환자 수정")
     public SuccessResponse<?> modifyPatientById(@RequestBody RegisterPatient registerPatient,
@@ -89,16 +62,32 @@ public class PatientController {
         return SuccessResponse.createSuccess();
     }
 
-    private PatientWithOperationDateDTO convertToPatientWithOperationDateDTO(Patient patient) {
-        List<Operation> operations = operationService.findAllByPatient(patient.getId());
-        List<OperationDTO> operationDTOs = operations.stream()
-                .map(OperationDTO::toEntity)
-                .collect(Collectors.toList());
+    @GetMapping("/patients")
+    @io.swagger.v3.oas.annotations.Operation(summary = "필터 환자 조회", description = "필터를 이용해 환자를 조회합니다.")
+    public SuccessResponse<?> findPatients(@RequestParam(required = false) FilterType filterType,
+                                           @RequestParam(required = false) String query,
+                                           @RequestParam(required = false) Integer page,
+                                           @RequestParam(required = false) Integer size,
+                                           @RequestParam(defaultValue = "DEFAULT") Opdate opDate,
+                                           @RequestParam(defaultValue = "ALL") CheckListStatus checkListStatus) {
+        int pageNumber = (page != null ? page : 1) - 1;
+        int pageSize = (size != null ? size : 10);
 
-        Operation recentOperation = operationService.findRecentOperationByPatientId(patient.getId());
-        boolean checkListCreatedToday = recentOperation != null && checkListService.checkIfAnyCheckListCreatedToday(recentOperation.getId());
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
 
-        return PatientWithOperationDateDTO.toEntity(patient, operationDTOs, checkListCreatedToday);
+        List<PatientWithOperationDateDTO> patientDTOs;
+        boolean queryPresent = StringUtils.hasText(query) && filterType != null;
+
+        List<Patient> patients = queryPresent ? getFilteredPatients(filterType, query, pageable).getContent()
+                : patientService.findAll(pageable).getContent();
+
+        patientDTOs = patients.stream()
+                .sorted(patientComparator(opDate))
+                .map(this::convertToPatientWithOperationDateDTO)
+                .filter(dto -> filterByCheckListStatus(dto, checkListStatus))
+                .toList();
+
+        return SuccessResponse.createSuccess(patientDTOs);
     }
 
     private Slice<Patient> getFilteredPatients(FilterType filterType, String query, Pageable pageable) {
@@ -109,32 +98,33 @@ public class PatientController {
         };
     }
 
-    @GetMapping("/patients/v2")
-    @io.swagger.v3.oas.annotations.Operation(summary = "전체 환자 조회", description = "전체 환자 목록을 조회하고 각 환자의 수술 정보를 포함합니다.")
-    public SuccessResponse<List<PatientWithOperationDateDTO>> findPatientsV2(@RequestParam(required = false) String query,
-                                                                             @RequestParam(required = false) Integer page,
-                                                                             @RequestParam(required = false) Integer size) {
-        Pageable pageable = PageRequest.of(page != null ? page - 1 : 0, size != null ? size : 10);
-        Set<Patient> patientsSet = new HashSet<>();
+    private Comparator<Patient> patientComparator(Opdate opDate) {
+        Comparator<Patient> comparator;
+        if (opDate == Opdate.NEWER) comparator = Comparator.comparing(Patient::getOperationDate).reversed();
+        else if (opDate == Opdate.OLDER) comparator = Comparator.comparing(Patient::getOperationDate);
+        else comparator = Comparator.comparing(Patient::getUpdatedAt).reversed();
 
-        if (query != null && !query.isEmpty()) {
-            try {
-                Long patientNumber = Long.parseLong(query);
-                patientsSet.addAll(patientService.findPatientsByPatientNumber(patientNumber, pageable).getContent());
-            } catch (NumberFormatException e) {
-                // query가 숫자가 아닐 경우 이름과 수술 방법으로 검색을 계속 진행합니다.
-                patientsSet.addAll(patientService.findPatientsByName(query, pageable).getContent());
-                patientsSet.addAll(operationService.findPatientsByOperationMethod(query, pageable).getContent());
-            }
+        return comparator;
+    }
+
+    private boolean filterByCheckListStatus(PatientWithOperationDateDTO dto, CheckListStatus checkListStatus) {
+        if (checkListStatus == CheckListStatus.WRITTEN) {
+            return dto.isCheckListCreatedToday();
+        } else if (checkListStatus == CheckListStatus.NOT_WRITTEN) {
+            return !dto.isCheckListCreatedToday();
         }
+        return true;
+    }
 
-        // 모든 환자를 조회한 후 중복 제거 및 정렬
-        List<PatientWithOperationDateDTO> patientDTOs = patientsSet.stream()
-                .sorted(Comparator.comparing(Patient::getUpdatedAt).reversed())
-                .map(this::convertToPatientWithOperationDateDTO)
-                .toList();
+    private PatientWithOperationDateDTO convertToPatientWithOperationDateDTO(Patient patient) {
+        List<OperationDTO> operationDTOs = operationService.findAllByPatient(patient.getId()).stream()
+                .map(OperationDTO::toEntity)
+                .collect(Collectors.toList());
 
-        return SuccessResponse.createSuccess(patientDTOs);
+        Operation recentOperation = operationService.findRecentOperationByPatientId(patient.getId());
+        boolean checkListCreatedToday = recentOperation != null && checkListService.checkIfAnyCheckListCreatedToday(recentOperation.getId());
+
+        return PatientWithOperationDateDTO.toEntity(patient, operationDTOs, checkListCreatedToday);
     }
 
 }
