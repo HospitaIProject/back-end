e7a0b8dc7ecb535f006f0fa2b4d4f87543be9b86
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-08-23 11:09:03 -0400

    feat: react rebuilt.

--- src/main/java/com/team/hospital/api/checkListItemDefault/CheckListItemDefaultService.java
+++ src/main/java/com/team/hospital/api/checkListItemDefault/CheckListItemDefaultService.java
@@ -1,9 +1,9 @@
 package com.team.hospital.api.checkListItemDefault;
 
 import com.team.hospital.api.checkListItemDefault.dto.WriteCheckListItemDefault;
-import com.team.hospital.api.checkListItemDefault.exception.CheckListItemDefaultAlreadyExistsException;
 import com.team.hospital.api.checkListItemDefault.exception.CheckListItemDefaultNotFoundException;
-import com.team.hospital.api.operation.enumType.OperationMethod;
+import com.team.hospital.api.operationType.OperationType;
+import com.team.hospital.api.operationType.OperationTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,19 +15,20 @@ import java.util.NoSuchElementException;
 public class CheckListItemDefaultService {
 
     private final CheckListItemDefaultRepository checkListItemDefaultRepository;
+    private final OperationTypeService operationTypeService;
 
     @Transactional
     public void save(WriteCheckListItemDefault write) {
-        if (existsByOperationMethod(write.getOperationMethod())) {
-            throw new CheckListItemDefaultAlreadyExistsException("이미 해당 operationMethod로 등록된 수술 항목 기본 값이 존재합니다.");
-        }
+//        if (existsByOperationType(write.getOperationType())) {
+//            throw new CheckListItemDefaultAlreadyExistsException("이미 해당 operationMethod로 등록된 수술 항목 기본 값이 존재합니다.");
+//        }
         CheckListItemDefault checkListItemDefault = CheckListItemDefault.toEntity(write);
         checkListItemDefaultRepository.save(checkListItemDefault);
     }
 
     @Transactional
     public void update(WriteCheckListItemDefault write) {
-        CheckListItemDefault checkListItemDefault = findByOperationMethod(write.getOperationMethod());
+        CheckListItemDefault checkListItemDefault = findByOperationTypeName(write.getOperationTypeName());
         checkListItemDefault.update(write);
     }
 
@@ -36,12 +37,20 @@ public class CheckListItemDefaultService {
         else throw new NoSuchElementException();
     }
 
-    public CheckListItemDefault findByOperationMethod(OperationMethod operationMethod) {
-        if (!existsByOperationMethod(operationMethod)) throw new CheckListItemDefaultNotFoundException("해당 operationMethod에 등록된 체크리스트 목록 기본값이 존재하지 않습니다.");
-        return checkListItemDefaultRepository.findByOperationMethod(operationMethod);
+    public CheckListItemDefault findByOperationType(OperationType operationType) {
+        if (!existsByOperationType(operationType)) throw new CheckListItemDefaultNotFoundException("해당 operationMethod에 등록된 체크리스트 목록 기본값이 존재하지 않습니다.");
+        return checkListItemDefaultRepository.findByOperationType(operationType);
     }
 
-    private boolean existsByOperationMethod(OperationMethod operationMethod) {
-        return checkListItemDefaultRepository.existsByOperationMethod(operationMethod);
+    public CheckListItemDefault findByOperationTypeName(String operationTypeName) {
+        OperationType operationType = operationTypeService.findByOperationTypeName(operationTypeName);
+        if (operationType.getCheckListItemDefault() != null) {
+            return operationType.getCheckListItemDefault();
+        }
+        return null;
+    }
+
+    private boolean existsByOperationType(OperationType operationType) {
+        return checkListItemDefaultRepository.existsByOperationType(operationType);
     }
 }
