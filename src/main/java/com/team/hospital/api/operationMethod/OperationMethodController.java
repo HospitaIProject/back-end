317335e5ad3f662dcdb7872376ac99ec495a4c9e
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-08-23 11:00:48 -0400

    feat: Add OperationMethodController to handle operation method retrieval
    - Implemented `OperationMethodController` with an endpoint to retrieve all operation methods.
    - Integrated `OperationMethodService` to fetch data from the database.
    - Added mapping from `OperationMethod` entities to `OperationMethodDTO` for API response.

---
+++ src/main/java/com/team/hospital/api/operationMethod/OperationMethodController.java
@@ -0,0 +1,27 @@
+package com.team.hospital.api.operationMethod;
+
+import com.team.hospital.api.apiResponse.SuccessResponse;
+import com.team.hospital.api.operationMethod.dto.OperationMethodDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class OperationMethodController {
+
+    private final OperationMethodService operationMethodService;
+
+    @GetMapping("/api/operationMethods")
+    public SuccessResponse<?> findAll() {
+        List<OperationMethod> all =
+                operationMethodService.findAll();
+        List<OperationMethodDTO> list = all.stream().map(OperationMethodDTO::toEntity).toList();
+
+        return SuccessResponse.createSuccess(list);
+    }
+}
