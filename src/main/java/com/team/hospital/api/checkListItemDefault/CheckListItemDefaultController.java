145f1202cc171c4495dc1272a89eb18f8433508d
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-08-23 11:06:59 -0400

    refactor: Consolidate and streamline CheckListItemDefaultController
    - Merged duplicated CheckListItemDefaultController classes.
    - Unified endpoint structure to handle both OperationType and OperationMethod parameters.
    - Simplified and clarified controller methods to ensure consistent API behavior.

--- src/main/java/com/team/hospital/api/checkListItemDefault/CheckListItemDefaultController.java
+++ src/main/java/com/team/hospital/api/checkListItemDefault/CheckListItemDefaultController.java
@@ -3,7 +3,7 @@ package com.team.hospital.api.checkListItemDefault;
 import com.team.hospital.api.apiResponse.SuccessResponse;
 import com.team.hospital.api.checkListItemDefault.dto.CheckListItemDefaultResponse;
 import com.team.hospital.api.checkListItemDefault.dto.WriteCheckListItemDefault;
-import com.team.hospital.api.operation.enumType.OperationMethod;
+import com.team.hospital.api.operationType.OperationType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +16,17 @@ public class CheckListItemDefaultController {
 
     private final CheckListItemDefaultService checkListItemDefaultService;
 
+    @GetMapping("/api/checkListItemDefault/{operationTypeName}")
+    @Operation(summary = "operationType의 이름으로 해당 수술 타입의 checkListItemDefault를 불러옵니다.")
+    public SuccessResponse<?> findByOperationTypeName(@PathVariable String operationTypeName) {
+        CheckListItemDefault checkListItemDefault = checkListItemDefaultService.findByOperationTypeName(operationTypeName);
+        return SuccessResponse.createSuccess(CheckListItemDefaultResponse.toEntity(checkListItemDefault));
+    }
+
     @GetMapping("/api/checkListItemDefault")
     @Operation(summary = "해당 수술 방법에 따른 CheckListItemDefault 엔티티를 검색하는 엔드포인트입니다.")
-    public SuccessResponse<?> find(@RequestParam OperationMethod operationMethod) {
-        CheckListItemDefault checkListItemDefault = checkListItemDefaultService.findByOperationMethod(operationMethod);
+    public SuccessResponse<?> find(@RequestParam OperationType operationType) {
+        CheckListItemDefault checkListItemDefault = checkListItemDefaultService.findByOperationType(operationType);
         return SuccessResponse.createSuccess(CheckListItemDefaultResponse.toEntity(checkListItemDefault));
     }
 
