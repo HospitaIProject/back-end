0dbed3248407556f85109cd0694dc4e9ab8cdd30
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-08-23 11:02:18 -0400

    feat: Add WriteOperationType DTO for OperationType creation
    - Added `WriteOperationType` DTO to handle the creation of new `OperationType` entities.
    - Included a `name` field to capture the name of the OperationType.
    - The DTO will be used in requests where a new OperationType needs to be registered.

---
+++ src/main/java/com/team/hospital/api/operationType/dto/WriteOperationType.java
@@ -0,0 +1,12 @@
+package com.team.hospital.api.operationType.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class WriteOperationType {
+
+    private String name;
+
+}
