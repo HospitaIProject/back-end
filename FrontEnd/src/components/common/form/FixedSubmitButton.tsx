53e7bce3aec11e296094cf615d75f6839c2e2a32
jinhyuek <jjh943202@naver.com>
2024-08-23 23:33:46 +0900

    Refactor: submit button 컴포넌트 -buttonColorClassName  prop 추가 -type: submit,button prop 추가

--- FrontEnd/src/components/common/form/FixedSubmitButton.tsx
+++ FrontEnd/src/components/common/form/FixedSubmitButton.tsx
@@ -2,18 +2,26 @@ type Props = {
     onClick?: () => void;
     label: string;
     className?: string;
+    buttonColorClassName?: string;
+    type?: 'submit' | 'button';
 };
 
-function FixedSubmitButton({ onClick, label, className }: Props) {
+function FixedSubmitButton({
+    onClick,
+    label,
+    className,
+    type = 'button',
+    buttonColorClassName = 'bg-blue-500',
+}: Props) {
     return (
         <div
-            className={`${className} sticky bottom-0 z-10 mt-auto flex w-full bg-white py-2 shadow shadow-transparent tablet:col-span-2`}
+            className={`${className} sticky bottom-0 z-10 mt-auto flex w-full bg-white shadow shadow-transparent tablet:col-span-2`}
         >
             {/* 모바일에서 그림자가 sticky속성 사용시 그림자가 생기는 문제 해결 */}
 
             <button
-                type="button"
-                className="w-full px-8 py-3 mx-auto text-white bg-blue-500 rounded-md max-w-screen-mobile hover:bg-blue-600"
+                type={type}
+                className={`w-full ${buttonColorClassName} mx-auto max-w-screen-mobile rounded-md px-8 py-3 text-white hover:bg-blue-600`}
                 onClick={onClick}
             >
                 {label}
