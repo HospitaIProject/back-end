25997db1d77050a9a05afdfaa722dede3342c2dd
jinhyuek <jjh943202@naver.com>
2024-08-29 01:29:53 +0900

    Refactor: createBrowserRouter router 경로 생성하는 방식으로 변경

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage/MainPage';
 import LoginPage from './LoginPage/LoginPage';
 import Layout from './Layout/Layout';
@@ -14,62 +13,46 @@ import CheckListViewPage from './PatientPage/CheckListsPage/components/CheckList
 import DailyCompliancePage from './PatientPage/ComplianceFormPage/DailyCompliancePage';
 import DefaultCheckListSettingPage from './DefaultCheckListSettingPage/DefaultCheckListSettingPage';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        element: <RQProviders />,
+        children: [
+            {
+                element: <Layout />,
+                children: [
+                    {
+                        element: <HeaderLayout />,
+                        children: [
+                            { path: '/', element: <MainPage /> },
+                            { path: '/operation-checkList/default-setting', element: <DefaultCheckListSettingPage /> },
+                            { path: '/patient/operation/list', element: <OperationListPage /> },
+                            { path: '/patient/checkLists', element: <CheckListsPage /> },
+                            { path: '/patient/checkLists/preview', element: <CheckListViewPage /> },
+                            { path: '/patient/new/info', element: <NewPatientFormPage /> },
+                            { path: '/patient/new/info/:patientId', element: <NewPatientFormPage /> },
+                            { path: '/patient/new/operation', element: <NewOperationInfoFormPage /> },
+                            { path: '/patient/new/operation/:operationId', element: <NewOperationInfoFormPage /> },
+                            { path: '/patient/form/compliance', element: <ComplianceFormPage /> },
+                            { path: '/patient/form/compliance/daily', element: <DailyCompliancePage /> },
+                            { path: '/patient/form/compliance/edit/:checkListId', element: <ComplianceFormPage /> },
+                            {
+                                path: '/patient/form/compliance/daily/edit/:checkListId',
+                                element: <DailyCompliancePage />,
+                            },
+                            { path: '/patient/new/complication', element: <NewComplicationFormPage /> },
+                        ],
+                    },
+                    { path: '/login', element: <LoginPage /> },
+                ],
+            },
+        ],
+    },
+]);
+
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route element={<RQProviders />}>
-                    {/* 전체 레이아웃 */}
-                    <Route element={<Layout />}>
-                        {/* 헤더 레이아웃 */}
-                        <Route element={<HeaderLayout />}>
-                            {/* 메인 페이지 */}
-                            <Route path="/" element={<MainPage />} />
-
-                            {/* 수술명에 따른  체크리스트아이템 기본 세팅 */}
-                            <Route
-                                path="/operation-checkList/default-setting"
-                                element={<DefaultCheckListSettingPage />}
-                            />
-
-                            {/* 수술정보 리스트 페이지 */}
-                            <Route path="/patient/operation/list" element={<OperationListPage />} />
-
-                            {/* 체크리스트목록 페이지 */}
-                            <Route path="/patient/checkLists" element={<CheckListsPage />} />
-                            {/* 체크리스트 항목 페이지 */}
-                            <Route path="/patient/checkLists/preview" element={<CheckListViewPage />} />
-
-                            {/* 환자 정보 등록 페이지 */}
-                            <Route path="/patient/new/info" element={<NewPatientFormPage />} />
-                            {/* 환자 정보 수정 페이지 */}
-                            <Route path="/patient/new/info/:patientId" element={<NewPatientFormPage />} />
-
-                            {/* 환자 수술정보 등록 페이지 */}
-                            <Route path="/patient/new/operation" element={<NewOperationInfoFormPage />} />
-                            {/* 환자 수술정보 수정 페이지 */}
-                            <Route path="/patient/new/operation/:operationId" element={<NewOperationInfoFormPage />} />
-
-                            {/* compilance 체크리스트 등록 페이지 */}
-                            <Route path="/patient/form/compliance" element={<ComplianceFormPage />} />
-                            <Route path="/patient/form/compliance/daily" element={<DailyCompliancePage />} />
-                            {/* compilance 체크리스트 수정 페이지 */}
-                            <Route path="/patient/form/compliance/edit/:checkListId" element={<ComplianceFormPage />} />
-                            <Route
-                                path="/patient/form/compliance/daily/edit/:checkListId"
-                                element={<DailyCompliancePage />}
-                            />
-
-                            {/* 합병증 등록 페이지 */}
-                            <Route path="/patient/new/complication" element={<NewComplicationFormPage />} />
-                        </Route>
-                        <Route path="/login" element={<LoginPage />} />
-                    </Route>
-                    {/* 추가 페이지 라우트 */}
-                </Route>
-            </Routes>
-        </Router>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
