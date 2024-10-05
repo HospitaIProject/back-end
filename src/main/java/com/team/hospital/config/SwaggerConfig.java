282491d8512e31ca08885aee27845387935f51ad
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-10-05 02:53:28 -0400

    feat: unused import statement deleted.

--- src/main/java/com/team/hospital/config/SwaggerConfig.java
+++ src/main/java/com/team/hospital/config/SwaggerConfig.java
@@ -7,8 +7,6 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class SwaggerConfig {
 
