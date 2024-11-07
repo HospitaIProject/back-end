9d8074966028ab1311e88644222322a126e1f7e0
Kim Dong Yeon <do121712@naver.com>
2024-11-08 04:38:57 +0900

    feat : 최근 삭제에 대한 Operation 조회 메소드 추가

--- src/main/java/com/team/hospital/api/operation/OperationRepository.java
+++ src/main/java/com/team/hospital/api/operation/OperationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,8 +18,22 @@ public interface OperationRepository extends JpaRepository<Operation, Long>  {
 
     List<Operation> findAll();
 
-    @Query("SELECT o FROM Operation o WHERE o.patient.id = :patientId ORDER BY o.createdAt DESC")
+    @Query("SELECT o FROM Operation o WHERE o.patient.id = :patientId AND o.isDeleted = false ORDER BY o.createdAt DESC")
     List<Operation> findOrderedAllByPatientId(Long patientId);
+
+    // 삭제 대기 상태의 Operation만 조회하는 메서드
+//    @Query("SELECT o FROM Operation o WHERE o.isDeleted = true")
+//    Slice<Operation> findAllMarkedForDeletion(Pageable pageable);
+    @Query("SELECT o FROM Operation o WHERE o.isDeleted = true")
+    List<Operation> findAllMarkedForDeletion();
+
+    // 30일 이상 지난 Operation을 조회하여 삭제 대기 상태로 전환하기 위한 메서드
+    @Query("SELECT o FROM Operation o WHERE o.updatedAt <= :date AND o.isDeleted = false")
+    List<Operation> findAllByOperationDateBeforeAndIsDeletedFalse(LocalDate date);
+
+    // 삭제 대기 상태로 변경된 후 30일 이상 지난 Operation을 조회하여 완전히 삭제하기 위한 메서드
+    @Query("SELECT o FROM Operation o WHERE o.isDeleted = true AND o.deletionRequestDate <= :date")
+    List<Operation> findAllByIsDeletedTrueAndDeletionRequestDateBefore(LocalDate date);
 
     @Query(value = "SELECT o.* FROM operation o " +
             "JOIN patient p ON o.patient_id = p.patient_id " +
