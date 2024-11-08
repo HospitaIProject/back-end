ee46cd42aa744be9220280b5dd60126335410aab
Kim Dong Yeon <do121712@naver.com>
2024-11-08 22:41:04 +0900

    refactor : List로 Operation 삭제, 복구 가능하도록 api 수정

--- src/main/java/com/team/hospital/api/operation/OperationController.java
+++ src/main/java/com/team/hospital/api/operation/OperationController.java
@@ -87,18 +87,18 @@ public class OperationController {
     }
 
     //최근 삭제 목록에서 진짜 삭제
-    @DeleteMapping("/api/cash/operation/{operationId}")
+    @DeleteMapping("/api/operation")
     @io.swagger.v3.oas.annotations.Operation(summary = "최근 삭제목록에서 operation 삭제")
-    public SuccessResponse<?> deleteOperation(@PathVariable Long operationId) {
-        operationService.delete(operationId);
+    public SuccessResponse<?> deleteOperation(@RequestBody CashOperationDTO cashOperationDTO) {
+        operationService.delete(cashOperationDTO);
         return SuccessResponse.createSuccess();
     }
 
     //최근 삭제 목록에서 복구
-    @PostMapping("/api/cash/operation/{operationId}")
+    @PostMapping("/api/operation")
     @io.swagger.v3.oas.annotations.Operation(summary = "최근 삭제목록에서 operation 복구")
-    public SuccessResponse<?> restoreOperation(@PathVariable Long operationId) {
-        operationService.restore(operationId);
+    public SuccessResponse<?> restoreOperation(@RequestBody CashOperationDTO cashOperationDTO) {
+        operationService.restore(cashOperationDTO);
         return SuccessResponse.createSuccess();
     }
 }
