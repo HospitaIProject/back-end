f68ffe4fb65c639353628b8e0fc5a37260b43605
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-11-09 17:20:44 -0500

    feat: Add UpdateIVDate DTO for IV line removal date updates
    - Introduced `UpdateIVDate` DTO to manage updates specific to `ivLineRemovalDate`.
    - Supports streamlined handling of IV line removal date changes in checklist operations.

---
+++ src/main/java/com/team/hospital/api/checkList/dto/UpdateIVDate.java
@@ -0,0 +1,10 @@
+package com.team.hospital.api.checkList.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class UpdateIVDate {
+    private LocalDate ivLineRemovalDate;
+}
