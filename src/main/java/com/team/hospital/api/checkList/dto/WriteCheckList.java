cc6415c70a5634fd5cd3d6b28fe5236ca35a79c0
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-11-09 17:21:43 -0500

    feat: Update fields to align with requirements
    - Adjusted field types and names in DTOs and entities.
    - Enhanced attribute handling for compliance with updated requirements.

--- src/main/java/com/team/hospital/api/checkList/dto/WriteCheckList.java
+++ src/main/java/com/team/hospital/api/checkList/dto/WriteCheckList.java
@@ -9,6 +9,11 @@ import java.time.LocalDate;
 @Getter
 public class WriteCheckList {
 
+    // 위장관 촉진약 복용 여부
+    private BooleanOption podOneGiStimulant; // POD 1day 위장관 촉진약 복용 여부
+    private BooleanOption podTwoGiStimulant; // POD 2day 위장관 촉진약 복용 여부
+    private BooleanOption podThreeGiStimulant; // POD 3day 위장관 촉진약 복용 여부
+
     // 껌씹기 여부
     private BooleanOption podOneGumChewing; // POD 1day 껌씹기 여부
     private BooleanOption podTwoGumChewing; // POD 2day 껌씹기 여부
@@ -25,13 +30,13 @@ public class WriteCheckList {
     private BooleanOption podThreeNonOpioidPainControl; // POD 3day non-opioid pain control 여부
 
     // 수술 후 3일이내 JP drain 제거 여부
-    private BooleanOption podOneJpDrainRemoval; // POD 1day 3일이내 JP drain 제거 여부
-    private BooleanOption podTwoJpDrainRemoval; // POD 2day 3일이내 JP drain 제거 여부
-    private BooleanOption podThreeJpDrainRemoval; // POD 3day 3일이내 JP drain 제거 여부
+//    private BooleanOption podOneJpDrainRemoval; // POD 1day 3일이내 JP drain 제거 여부
+//    private BooleanOption podTwoJpDrainRemoval; // POD 2day 3일이내 JP drain 제거 여부
+//    private BooleanOption podThreeJpDrainRemoval; // POD 3day 3일이내 JP drain 제거 여부
 
     // 수술 후 3일이내 IV line 제거 여부
-    private BooleanOption podOneIvLineRemoval; // POD 1day 3일이내 IV line 제거 여부
-    private BooleanOption podTwoIvLineRemoval; // POD 2day 3일이내 IV line 제거 여부
+//    private BooleanOption podOneIvLineRemoval; // POD 1day 3일이내 IV line 제거 여부
+//    private BooleanOption podTwoIvLineRemoval; // POD 2day 3일이내 IV line 제거 여부
     private BooleanOption podThreeIvLineRemoval; // POD 3day 3일이내 IV line 제거 여부
 
     // Pod Exercise
@@ -52,14 +57,10 @@ public class WriteCheckList {
     private LocalDate dayOfCheckList;
 
     // 비고 필드
-    // Pod Exercise
-    private String podOneExercise_remarks;
-    private String podTwoExercise_remarks;
-    private String podThreeExercise_remarks;
-
-    // Pod Meal
-    private String podOneMeal_remarks;
-    private String podTwoMeal_remarks;
+    // 위장관 촉진약 복용 여부
+    private String podOneGiStimulant_remarks; // POD 1day 위장관 촉진약 복용 여부
+    private String podTwoGiStimulant_remarks; // POD 2day 위장관 촉진약 복용 여부
+    private String podThreeGiStimulant_remarks; // POD 3day 위장관 촉진약 복용 여부
 
     // 껌씹기 여부
     private String podOneGumChewing_remarks; // POD 1day 껌씹기 여부
@@ -77,13 +78,22 @@ public class WriteCheckList {
     private String podThreeNonOpioidPainControl_remarks; // POD 3day non-opioid pain control 여부
 
     // 수술 후 3일이내 JP drain 제거 여부
-    private String podOneJpDrainRemoval_remarks; // POD 1day 3일이내 JP drain 제거 여부
-    private String podTwoJpDrainRemoval_remarks; // POD 2day 3일이내 JP drain 제거 여부
-    private String podThreeJpDrainRemoval_remarks; // POD 3day 3일이내 JP drain 제거 여부
+//    private String podOneJpDrainRemoval_remarks; // POD 1day 3일이내 JP drain 제거 여부
+//    private String podTwoJpDrainRemoval_remarks; // POD 2day 3일이내 JP drain 제거 여부
+//    private String podThreeJpDrainRemoval_remarks; // POD 3day 3일이내 JP drain 제거 여부
 
     // 수술 후 3일이내 IV line 제거 여부
-    private String podOneIvLineRemoval_remarks; // POD 1day 3일이내 IV line 제거 여부
-    private String podTwoIvLineRemoval_remarks; // POD 2day 3일이내 IV line 제거 여부
+//    private String podOneIvLineRemoval_remarks; // POD 1day 3일이내 IV line 제거 여부
+//    private String podTwoIvLineRemoval_remarks; // POD 2day 3일이내 IV line 제거 여부
     private String podThreeIvLineRemoval_remarks; // POD 3day 3일이내 IV line 제거 여부
+
+    // Pod Exercise
+    private String podOneExercise_remarks;
+    private String podTwoExercise_remarks;
+    private String podThreeExercise_remarks;
+
+    // Pod Meal
+    private String podOneMeal_remarks;
+    private String podTwoMeal_remarks;
 
 }
