d62e40bbe3be620ced782ad42020792f75c31740
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-11-09 17:14:01 -0500

    refactor: Update CheckListDailyDTO - add giStimulant field and remove jpDrainRemoval field
    - Added `giStimulant` field to track gastrointestinal stimulant usage.
    - Removed `jpDrainRemoval` field from daily checklist as it's no longer applicable.
    - Updated `createCheckListDailyDTO` method to include `giStimulant` field logic.

--- src/main/java/com/team/hospital/api/checkList/dto/CheckListDailyDTO.java
+++ src/main/java/com/team/hospital/api/checkList/dto/CheckListDailyDTO.java
@@ -12,19 +12,24 @@ import static com.team.hospital.api.checkList.enumType.BooleanOption.YES;
 @Getter
 public class CheckListDailyDTO {
     @Builder.Default
+    private boolean giStimulant = false;
+    @Builder.Default
     private boolean gumChewing = false;
     @Builder.Default
     private boolean ivFluidRestriction = false;
     @Builder.Default
     private boolean nonOpioidPainControl = false;
-    @Builder.Default
-    private boolean jpDrainRemoval = false;
+//    @Builder.Default
+//    private boolean jpDrainRemoval = false;
     @Builder.Default
     private boolean ivLineRemoval = false;
 
     public static CheckListDailyDTO createCheckListDailyDTO(List<CheckList> checkLists) {
         CheckListDailyDTO.CheckListDailyDTOBuilder builder = CheckListDailyDTO.builder();
         for (CheckList checkList : checkLists) {
+            if (checkList.getPodOneGiStimulant().getOption() == YES || checkList.getPodTwoGiStimulant().getOption() == YES || checkList.getPodThreeGiStimulant().getOption() == YES) {
+                builder.giStimulant(true);
+            }
             if (checkList.getPodOneGumChewing().getOption() == YES || checkList.getPodTwoGumChewing().getOption() == YES || checkList.getPodThreeGumChewing().getOption() == YES) {
                 builder.gumChewing(true);
             }
@@ -34,9 +39,9 @@ public class CheckListDailyDTO {
             if (checkList.getPodOneNonOpioidPainControl().getOption() == YES || checkList.getPodTwoNonOpioidPainControl().getOption() == YES || checkList.getPodThreeNonOpioidPainControl().getOption() == YES) {
                 builder.nonOpioidPainControl(true);
             }
-            if (checkList.getPodOneJpDrainRemoval().getOption() == YES || checkList.getPodTwoJpDrainRemoval().getOption() == YES || checkList.getPodThreeJpDrainRemoval().getOption() == YES) {
-                builder.jpDrainRemoval(true);
-            }
+//            if (checkList.getPodOneJpDrainRemoval().getOption() == YES || checkList.getPodTwoJpDrainRemoval().getOption() == YES || checkList.getPodThreeJpDrainRemoval().getOption() == YES) {
+//                builder.jpDrainRemoval(true);
+//            }
             if (checkList.getPodThreeIvLineRemoval().getOption() == YES) {
                 builder.ivLineRemoval(true);
             }
