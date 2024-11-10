bebe9cef7b13591063392f4c5c09bfa7c5137155
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2024-11-10 18:44:52 -0500

    feat: delete iVLineRemoval retrieval on ExcelExportService

--- src/main/java/com/team/hospital/excel/ExcelExportService.java
+++ src/main/java/com/team/hospital/excel/ExcelExportService.java
@@ -386,8 +386,8 @@ public class ExcelExportService {
                 setStringCellValueSafe(row, 29, () -> convertDateToString(checkListAfter.getJpDrainRemoval().getRemovedDate()));      //JP drain 제거일
                 setIntCellValueSafe(row, 30, () -> checkListAfter.getCatheterRemoval().getOption().getNum());                   //Urinary catheter 수술실에서 제거
                 setStringCellValueSafe(row, 31, () -> convertDateToString(checkListAfter.getCatheterRemoval().getRemovedDate()));     //Urinary catheter 제거 날짜
-                setIntCellValueSafe(row, 32, () -> checkListAfter.getIvLineRemoval().getOption().getNum());                     //POD#3 이후 IV 라인제거
-                setStringCellValueSafe(row, 33, () -> convertDateToString(checkListAfter.getIvLineRemoval().getRemovedDate()));        //IV 라인 제거 날짜
+//                setIntCellValueSafe(row, 32, () -> checkListAfter.getIvLineRemoval().getOption().getNum());                     //POD#3 이후 IV 라인제거
+//                setStringCellValueSafe(row, 33, () -> convertDateToString(checkListAfter.getIvLineRemoval().getRemovedDate()));        //IV 라인 제거 날짜
                 setIntCellValueSafe(row, 34, () -> checkListAfter.getPostExercise().getOption().getNum());                      //OP day 운동
                 setIntCellValueSafe(row, 37, () -> checkListAfter.getPostMeal().getOption().getNum()); //OP day Diet
 
@@ -428,7 +428,7 @@ public class ExcelExportService {
                     }
 
                     //둘째날
-                    if (c.getPodThreeGumChewing() != null) {
+                    if (c.getPodTwoGumChewing() != null) {
                         gumBooleanList.add(c.getPodTwoGumChewing().getOption().getNum());
                         //laxativesList.add();
                         fluid_limitList.add(c.getPodTwoIvFluidRestriction().getOption().getNum());
@@ -443,7 +443,7 @@ public class ExcelExportService {
                         postop_pain_controlList.add(c.getPodThreeNonOpioidPainControl().getOption().getNum());
                     }
 
-                    setIntCellValueSafe(row, 28, () -> c.getPodOneJpDrainRemoval().getOption().getNum());       //POD#3 -> 로 변경 POD#1 이후 JP 제거했는지
+//                    setIntCellValueSafe(row, 28, () -> c.getPodOneJpDrainRemoval().getOption().getNum());       //POD#3 -> 로 변경 POD#1 이후 JP 제거했는지
 
                     if (c.getPodOneExercise() != null) {
                         setIntCellValueSafe(row, 35, () -> c.getPodOneExercise().getOption().getNum());
