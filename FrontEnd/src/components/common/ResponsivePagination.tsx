f4c7cff9ef4787db59cde835efbbf2ac268cc213
jinhyuek <jjh943202@naver.com>
2024-11-16 16:51:00 +0900

    Refactor: 페이지네이션 border 색상변경

--- FrontEnd/src/components/common/ResponsivePagination.tsx
+++ FrontEnd/src/components/common/ResponsivePagination.tsx
@@ -4,10 +4,10 @@ import MobilePagination from './MobilePagination';
 function ResponsivePagination({ pageSize = 1 }: { pageSize?: number }) {
     return (
         <>
-            <div className="flex flex-row w-full mt-auto border-t border-gray-100 mobile:hidden">
+            <div className="flex flex-row w-full mt-auto border-t border-gray-200 mobile:hidden">
                 <MobilePagination pageSize={pageSize} />
             </div>
-            <div className="flex-row hidden w-full mt-auto border-t border-gray-100 mobile:flex">
+            <div className="flex-row hidden w-full mt-auto border-t border-gray-200 mobile:flex">
                 {/* <DesktopPagination pageSize={pageSize} /> */}
                 <MobilePagination pageSize={pageSize} />
             </div>
